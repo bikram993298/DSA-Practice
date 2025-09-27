965e2c1bdcd4a66fb06c93290d536c475906e731
sparrow <150315437+bikram993298@users.noreply.github.com>
2025-09-27 10:57:49 +0530

    Time: 0 ms (100%), Space: 41.6 MB (99.46%) - LeetHub

---
+++ 3071-minimum-operations-to-write-the-letter-y-on-a-grid/3071-minimum-operations-to-write-the-letter-y-on-a-grid.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    bool check(int i, int j, vector<vector<int>>& grid) {
+    int n = grid.size();
+    int mid = n / 2;
+
+    // Top part: two diagonals leading to the center
+    if (i < mid && (i == j || i + j == n - 1)) return true;
+
+    // Bottom part: vertical line from center down
+    if (i >= mid && j == mid) return true;
+
+    return false;
+}
+
+    int minimumOperationsToWriteY(vector<vector<int>>& grid) {
+        int count0=0;
+        int count1=0;
+        int count2=0;
+        int not0=0;
+        int not1=0;
+        int not2=0;
+        int n=grid.size();
+        int m=grid[0].size();
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(check(i,j,grid)){
+                    if(grid[i][j]==0)count0++;
+                    else if(grid[i][j]==1)count1++;
+                    else count2++;
+                }else{
+                    if(grid[i][j]==0)not0++;
+                    else if(grid[i][j]==1)not1++;
+                    else not2++;
+                }
+            }
+        }
+        int mini=INT_MAX;
+        //for taking 0
+        int y=count0+count1+count2;
+        int noty=not0+not1+not2;
+        mini=min(mini,y-count0+(noty-max(not1,not2)));
+        //for taking 1;
+         mini=min(mini,y-count1+(noty-max(not0,not2)));
+        //for takong 2;
+         mini=min(mini,y-count2+(noty-max(not1,not0)));
+
+
+         return mini;
+        
+    }
+};
