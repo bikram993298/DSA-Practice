8e49cc2bc0484caa785ae717837768833dcc2c24
sparrow <150315437+bikram993298@users.noreply.github.com>
2025-10-19 15:43:30 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.cpp
+++ 0010-regular-expression-matching/0010-regular-expression-matching.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
-    bool isMatch(string s, string p) {
-        vector<vector<int>> dp(s.size()+1, vector<int>(p.size()+1, -1));
-        return solve(0, 0, s, p, dp);
-    }
-
     bool solve(int i, int j, string &s, string &p, vector<vector<int>> &dp) {
-        if (j == p.size()) return i == s.size();  // both exhausted
+        if (j == p.size()) return i == s.size();  
         if (dp[i][j] != -1) return dp[i][j];
 
-        bool first_match = (i < s.size() && (s[i] == p[j] || p[j] == '.'));
-
+        bool prev_match = (i > 0 && j > 0 && (s[i - 1] == p[j - 1] || p[j - 1] == '.'));
         bool ans;
+
         if (j + 1 < p.size() && p[j + 1] == '*') {
             // Skip or use '*'
-            ans = solve(i, j + 2, s, p, dp) || (first_match && solve(i + 1, j, s, p, dp));
+            ans = solve(i, j + 2, s, p, dp) || (prev_match && solve(i + 1, j, s, p, dp));
         } else {
             // Direct match
-            ans = first_match && solve(i + 1, j + 1, s, p, dp);
+            ans = prev_match && solve(i + 1, j + 1, s, p, dp);
         }
 
         return dp[i][j] = ans;
+    }
+
+    bool isMatch(string s, string p) {
+        vector<vector<int>> dp(s.size() + 1, vector<int>(p.size() + 1, -1));
+        return solve(0, 0, s, p, dp);
     }
 };
