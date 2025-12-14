d2db5141c1e9a33bee3d15f4b6fc4a34a4f3bcfb
sparrow <150315437+bikram993298@users.noreply.github.com>
2025-12-14 10:17:28 +0530

    Time: 1790 ms (20.82%), Space: 17.3 MB (94.72%) - LeetHub

--- 1027-longest-arithmetic-subsequence/1027-longest-arithmetic-subsequence.cpp
+++ 1027-longest-arithmetic-subsequence/1027-longest-arithmetic-subsequence.cpp
@@ -5,7 +5,7 @@ public:
     
     int solve(vector<int>& nums, int i, int diff) {
         
-        if(i == n-1)
+        if(i < 0)
             return 0;
         
         if(t[i][diff+501] != -1)
@@ -13,9 +13,9 @@ public:
         
         int ans = 0;
         
-        for(int k = i+1; k<n; k++) {
+        for(int k = i-1; k >= 0; k--) {
             
-            if(nums[k] - nums[i] == diff)
+            if(nums[i] - nums[k] == diff)
                 ans = max(ans, 1 + solve(nums, k, diff));
             
         }
@@ -36,7 +36,7 @@ public:
         for(int i = 0; i<n; i++) {
             for(int j = i+1; j < n; j++) {
                 
-                result = max(result, 1 + solve(nums, i, nums[j] - nums[i]));
+                result = max(result, 2 + solve(nums, i, nums[j] - nums[i]));
                 
             }
         }
