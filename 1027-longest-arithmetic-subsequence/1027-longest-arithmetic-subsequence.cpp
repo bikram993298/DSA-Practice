b873946f9202be2a77aa27c236ee5a0c37900936
sparrow <150315437+bikram993298@users.noreply.github.com>
2025-12-14 10:42:45 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1027-longest-arithmetic-subsequence/1027-longest-arithmetic-subsequence.cpp
+++ 1027-longest-arithmetic-subsequence/1027-longest-arithmetic-subsequence.cpp
@@ -5,7 +5,7 @@ public:
     
     int solve(vector<int>& nums, int i, int diff) {
         
-        if(i < 0)
+        if(i == n-1)
             return 0;
         
         if(t[i][diff+501] != -1)
@@ -13,9 +13,9 @@ public:
         
         int ans = 0;
         
-        for(int k = i-1; k >= 0; k--) {
+        for(int k = i+1; k<n; k++) {
             
-            if(nums[i] - nums[k] == diff)
+            if(nums[k] - nums[i] == diff)
                 ans = max(ans, 1 + solve(nums, k, diff));
             
         }
@@ -36,7 +36,7 @@ public:
         for(int i = 0; i<n; i++) {
             for(int j = i+1; j < n; j++) {
                 
-                result = max(result, 2 + solve(nums, i, nums[j] - nums[i]));
+                result = max(result, 1 + solve(nums, i, nums[j] - nums[i]));
                 
             }
         }
@@ -44,3 +44,5 @@ public:
         return result;
     }
 };
+//in brute force use two for loop i 0 to n and j i+1 to n ,,then agagin k from j+1 to n ,if diff equal len++,,count maximum lent,,,,
+//in recursion what we do we pass solve by index and avalable diff,then  again for loop i+1 to n checl diff equal again recursivel  call solve,,if i>n return 0 ,,,but complexcity n^3,,so bottom up,,here is memo trick that diff can be negative and diff can be upto 500,so in -500 can also happen to initialie dp state +5001 to remove negative
