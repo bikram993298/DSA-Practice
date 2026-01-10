df6e169c3e0087c511d5f246f9c6f97d65ab1134
sparrow <150315437+bikram993298@users.noreply.github.com>
2026-01-11 01:57:25 +0530

    Time: 3 ms (61.07%), Space: 14 MB (34.79%) - LeetHub

--- 0719-find-k-th-smallest-pair-distance/0719-find-k-th-smallest-pair-distance.cpp
+++ 0719-find-k-th-smallest-pair-distance/0719-find-k-th-smallest-pair-distance.cpp
@@ -1,35 +1,85 @@
+// class Solution {
+// public:
+//     int smallestDistancePair(vector<int>& nums, int k) {
+//         int n = nums.size();
+//         int max_element = 0;
+
+//         for(int i=0;i<n;i++)
+//         {
+//             max_element = max(max_element,nums[i]);
+//         }
+
+//         vector<int> pair_diff(max_element +1 ,0);
+
+//         for(int i=0;i<n;i++)
+//         {
+//             for(int j = i+1;j<n;j++)
+//             {
+//                 int diff = abs(nums[i]-nums[j]);
+//                 pair_diff[diff] +=1;
+//             }
+//         }
+
+
+//         for(int i=0;i<max_element+1;i++)
+//         {
+//             k -= pair_diff[i];
+//             if(k <= 0)
+//             {
+//                 return i;
+//             }
+//         }
+
+//         return 0;
+//     }
+// };
 class Solution {
 public:
+    int countPair(vector<int>& nums , int diff)
+    {
+        int n = nums.size();
+        int i=0;
+        int j = i+1;
+        int countAllPair = 0;
+        while(i < n && j<n)
+        {
+            if(abs(nums[i] -nums[j]) <= diff)
+            {
+                countAllPair += (j-i);
+                j++;
+            }else
+            {
+                i++;   
+            }
+        }
+
+        return countAllPair;
+    }
     int smallestDistancePair(vector<int>& nums, int k) {
         int n = nums.size();
-        int max_element = 0;
 
-        for(int i=0;i<n;i++)
+        sort(nums.begin(),nums.end());
+
+        int low = 0;
+        int high = abs(nums[0] - nums[n-1]);
+        int ans = 0;
+
+        while(low <= high)
         {
-            max_element = max(max_element,nums[i]);
-        }
+            int mid = (low + high)/2;
 
-        vector<int> pair_diff(max_element +1 ,0);
+            int count = countPair(nums , mid);
 
-        for(int i=0;i<n;i++)
-        {
-            for(int j = i+1;j<n;j++)
+            if(count < k)
             {
-                int diff = abs(nums[i]-nums[j]);
-                pair_diff[diff] +=1;
+                low = mid+1;
+            }else{
+                ans = mid;
+                high = mid-1;
             }
         }
 
 
-        for(int i=0;i<max_element+1;i++)
-        {
-            k -= pair_diff[i];
-            if(k <= 0)
-            {
-                return i;
-            }
-        }
-
-        return 0;
+        return ans;
     }
 };
