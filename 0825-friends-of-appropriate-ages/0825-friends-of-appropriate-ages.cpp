bf6496482102af607ebca2a09d8f3b5847ca2e00
sparrow <150315437+bikram993298@users.noreply.github.com>
2026-01-12 22:15:57 +0530

    Time: 11 ms (37.5%), Space: 41.2 MB (67.61%) - LeetHub

--- 0825-friends-of-appropriate-ages/0825-friends-of-appropriate-ages.cpp
+++ 0825-friends-of-appropriate-ages/0825-friends-of-appropriate-ages.cpp
@@ -1,42 +1,30 @@
 class Solution {
 public:
     int numFriendRequests(std::vector<int>& ages) {
-        std::sort(ages.begin(), ages.end());
-        int n = ages.size();
+        sort(ages.begin(), ages.end());
         int totalRequests = 0;
-        
-        int left = 0;
-        int right = 0;
-        
-        for (int i = 0; i < n; ++i) {
-            int x_age = ages[i];
-            
-            // Age too young to send any requests
-            if (x_age < 15) continue;
 
-            // Move 'left' to the first valid y: age[y] > 0.5 * x_age + 7
-            while (left < n && ages[left] <= 0.5 * x_age + 7) {
-                left++;
-            }
-            
-            // Move 'right' to the last valid y: age[y] <= x_age
-            // Note: Since the array is sorted, we include people with the same age
-            while (right + 1 < n && ages[right + 1] <= x_age) {
-                right++;
-            }
-            
-            // The range [left, right] contains all valid y's for this x.
-            // We subtract 1 because x cannot friend themselves.
+        for (int i = 0; i < ages.size(); ++i) {
+            int x_age = ages[i];
+            if (x_age < 15) continue; // Rule 1 makes requests impossible below age 15
+
+            // Find first y where age[y] > 0.5 * x_age + 7
+            // upper_bound finds the first element > value
+            auto it_low = upper_bound(ages.begin(), ages.end(), 0.5 * x_age + 7);
+            int left = distance(ages.begin(), it_low);
+
+            // Find first y where age[y] > x_age
+            auto it_high = upper_bound(ages.begin(), ages.end(), x_age);
+            int right =distance(ages.begin(), it_high) - 1;
+            //distance gives 0 based index form iterator
+
             if (right >= left) {
                 totalRequests += (right - left);
             }
         }
-        
         return totalRequests;
     }
 };
-
-
 //age[y] <= 0.5 * age[x] + 7
 //age[y] > age[x]
 //age[y] > 100 && age[x] < 100 last ine is rebundant because of 2nd condition and
