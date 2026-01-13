6be741ce8f80fb0592acecb501bf629102a53afa
sparrow <150315437+bikram993298@users.noreply.github.com>
2026-01-13 15:30:19 +0530

    Time: 0 ms (100%), Space: 10.9 MB (70.34%) - LeetHub

---
+++ 0522-longest-uncommon-subsequence-ii/0522-longest-uncommon-subsequence-ii.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool isSubsequence(string &a, string &b) {
+        int i = 0, j = 0;
+        while (i < a.size() && j < b.size()) {
+            if (a[i] == b[j]) i++;
+            j++;
+        }
+        return i == a.size();
+    }
+
+    int findLUSlength(vector<string>& strs) {
+        int n = strs.size();
+        
+        sort(strs.begin(), strs.end(), [](string &a, string &b) {
+            return a.size() > b.size();
+        });
+
+        for (int i = 0; i < n; i++) {
+            bool uncommon = true;
+            for (int j = 0; j < n; j++) {
+                if (i == j) continue;
+                if (isSubsequence(strs[i], strs[j])) {
+                    uncommon = false;
+                    break;
+                }
+            }
+            if (uncommon) return strs[i].size();
+        }
+
+        return -1;
+    }
+};
