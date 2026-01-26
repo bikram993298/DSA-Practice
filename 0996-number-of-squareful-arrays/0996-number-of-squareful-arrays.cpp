d205e07607a5595dc4f86faf0df4ced5a56ea8d8
sparrow <150315437+bikram993298@users.noreply.github.com>
2026-01-26 13:37:02 +0530

    Time: 0 ms (100%), Space: 10.1 MB (75.47%) - LeetHub

--- 0996-number-of-squareful-arrays/0996-number-of-squareful-arrays.cpp
+++ 0996-number-of-squareful-arrays/0996-number-of-squareful-arrays.cpp
@@ -1,37 +1,42 @@
 class Solution {
 public:
-    bool check(int x) {
-        int b = sqrt(x);
-        return b * b == x;
+    bool check(int i){
+        int b=sqrt(i);
+        return b*b==i;
     }
+    int solve(int i,vector<int> & nums,vector<bool>& used,int prev){
+        int n=nums.size();
+        if(i==n){
+            return 1;
 
-    int solve(int i, vector<int>& nums, vector<bool>& used, int prev) {
-        int n = nums.size();
-        if (i == n) return 1;
-
-        int ans = 0;
-
-        for (int j = 0; j < n; j++) {
-            if (used[j]) continue;
-
-            // skip duplicates
-            if (j > 0 && nums[j] == nums[j - 1] && !used[j - 1]) continue;
-
-            if (prev == -1 || check(nums[j] + prev)) {
-                used[j] = true;
-                ans += solve(i + 1, nums, used, nums[j]);
-                used[j] = false;
-            }
         }
+        int ans=0;
+    
+    for(int j=0;j<n;j++){
+        if(used[j])continue;
+         if (j> 0 && nums[j] == nums[j - 1] && !used[j - 1]) continue;
+       if(prev == -1 || check(nums[j] + prev))
+{
+            used[j] = true;
 
-        return ans;
+
+            ans=ans+solve(i+1,nums,used,nums[j]);
+           used[j] = false;
+
+
+        }
     }
 
+    return ans;
+        
+    }
     int numSquarefulPerms(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        int n = nums.size();
-        vector<bool> used(n, false);
+        int n=nums.size();
+        sort(nums.begin(),nums.end());
 
-        return solve(0, nums, used, -1);
+        vector<bool> used(n,false);
+        int ans=solve(0,nums,used,-1);
+        return ans;
+        
     }
 };
