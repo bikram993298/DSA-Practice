c5b0fb3ee6879bbe274a85e7f63ccd3a63956831
sparrow <150315437+bikram993298@users.noreply.github.com>
2026-01-28 12:36:53 +0530

    Time: 130 ms (53.18%), Space: 270.9 MB (42.7%) - LeetHub

--- 3608-minimum-time-for-k-connected-components/3608-minimum-time-for-k-connected-components.cpp
+++ 3608-minimum-time-for-k-connected-components/3608-minimum-time-for-k-connected-components.cpp
@@ -1,66 +1,115 @@
-class Solution {
+
+class DSU {
 public:
-    class DSU {
-    public:
-        vector<int> parent, size;
-        int components;
-        
-        DSU(int n) {
-            parent.resize(n);
-            size.assign(n, 1);
-            iota(parent.begin(), parent.end(), 0);
-            components = n;
-        }
-        
-        int find(int x) {
-            if (parent[x] != x)
-                parent[x] = find(parent[x]);
-            return parent[x];
-        }
-        
-        bool unite(int a, int b) {
-            a = find(a);
-            b = find(b);
-            if (a == b) return false;
-            if (size[a] < size[b]) swap(a, b);
-            parent[b] = a;
-            size[a] += size[b];
-            components--;
-            return true;
-        }
-        
-        bool split(int a, int b) {
-            // splitting here just means we don't unite on this edge
-            // we track components with reverse logic outside
-            return false;
-        }
-    };
+    vector<int> parent, size;
+    int components;
 
-    int minTime(int n, vector<vector<int>>& edges, int k) {
-        // Start with all nodes in 1 component
-        DSU dsu(n);
-        // First union all edges so dsu.components = 1
-        for (auto &e : edges)
-            dsu.unite(e[0], e[1]);
+    DSU(int n) {
+        parent.resize(n);
+        size.assign(n, 1);
+        iota(parent.begin(), parent.end(), 0);
+        components = n;
+    }
 
-        // Sort edges descending (largest first)
-        sort(edges.begin(), edges.end(), [&](auto &a, auto &b){
-            return a[2] > b[2];
-        });
+    int find(int x) {
+        if (parent[x] != x) parent[x] = find(parent[x]);
+        return parent[x];
+    }
 
-        int currComps = 1;
-        int result = 0;
-
-        for (auto &e : edges) {
-            if (currComps == k) break;
-            // Splitting this edge increases components
-            currComps++;
-            result = max(result, e[2]);
-        }
-
-        // If we never reached k components, impossible
-        if (currComps != k) return -1;
-
-        return result;
+    void unite(int a, int b) {
+        a = find(a);
+        b = find(b);
+        if (a == b) return;
+        if (size[a] < size[b]) swap(a, b);
+        parent[b] = a;
+        size[a] += size[b];
+        components--;
     }
 };
+
+class Solution {
+public:
+    // true if after removing edges with time <= T (i.e. keeping edges with time > T)
+    // the number of components is >= k
+    bool canReduceToK(int n, vector<vector<int>>& edges, int k, int T) {
+        DSU dsu(n);
+        for (auto &e : edges) {
+            // edge is active AFTER time T iff its failure time > T
+            if (e[2] > T)
+                dsu.unite(e[0], e[1]);
+        }
+        return dsu.components >= k;
+        //  5 min me 4 component ban gaya ,,muje cahihe k=3 component,,it is psoosible ans
+    }
+
+    int minTime(int n, vector<vector<int>>& edges, int k) {
+        if (k > n) return -1; // impossible
+
+        int low = 0, high = 0;
+        for (auto &e : edges)
+            high = max(high, e[2]);
+
+        int ans = -1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (canReduceToK(n, edges, k, mid)) {
+                ans = mid;
+                high = mid - 1; // try smaller T
+            } else {
+                low = mid + 1;  // need larger T
+            }
+        }
+        return ans;
+    }
+};
+
+// class Solution {
+// public:
+//     class DSU {
+//     public:
+//         vector<int> parent, size;
+//         int components;
+
+//         DSU(int n) {
+//             parent.resize(n);
+//             size.assign(n, 1);
+//             iota(parent.begin(), parent.end(), 0);
+//             components = n;
+//         }
+
+//         int find(int x) {
+//             if (parent[x] != x)
+//                 parent[x] = find(parent[x]);
+//             return parent[x];
+//         }
+
+//         void unite(int a, int b) {
+//             a = find(a);
+//             b = find(b);
+//             if (a == b) return;
+//             if (size[a] < size[b]) swap(a, b);
+//             parent[b] = a;
+//             size[a] += size[b];
+//             components--;
+//         }
+//     };
+
+//     int minTime(int n, vector<vector<int>>& edges, int k) {
+
+//         DSU dsu(n);
+
+//         sort(edges.begin(), edges.end(),
+//              [](auto &a, auto &b) {
+//                  return a[2] < b[2];
+//              });
+
+//         for (auto &e : edges) {
+//             dsu.unite(e[0], e[1]);
+//             if (dsu.components == k)   // ✅ EXACT match
+//                 return e[2];
+//         }
+
+//         return -1;  // not possible
+//     }
+// };
+
