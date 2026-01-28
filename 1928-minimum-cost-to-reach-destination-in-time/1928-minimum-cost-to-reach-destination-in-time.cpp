cc5595d6a281cb5406ae0eaab79e992b63cf07dd
sparrow <150315437+bikram993298@users.noreply.github.com>
2026-01-28 13:03:29 +0530

    Time: 1606 ms (5.02%), Space: 128.1 MB (15.26%) - LeetHub

--- 1928-minimum-cost-to-reach-destination-in-time/1928-minimum-cost-to-reach-destination-in-time.cpp
+++ 1928-minimum-cost-to-reach-destination-in-time/1928-minimum-cost-to-reach-destination-in-time.cpp
@@ -1,56 +1,66 @@
-#include <vector>
-#include <queue>
-#include <tuple>
-
-using namespace std;
-
 class Solution {
 public:
     int minCost(int maxTime, vector<vector<int>>& edges, vector<int>& passingFees) {
         int n = passingFees.size();
-        vector<vector<pair<int, int>>> adj(n);
-        for (auto& e : edges) {
+        
+       //here we have to give minimum cosr pat  but if total time exceed we have to cnotiue,,so in prioery ques,,cots,,time,node
+        vector<vector<pair<int,int>>> adj(n);
+        for (auto &e : edges) {
             adj[e[0]].push_back({e[1], e[2]});
             adj[e[1]].push_back({e[0], e[2]});
         }
 
         const int INF = 1e9;
-        // minTime[city] will store the minimum time taken to reach 'city' 
-        // with a cost better than or equal to what we've seen.
-        vector<int> minTime(n, INF);
+        vector<vector<int>> dp(n, vector<int>(maxTime + 1, INF));
+        // we cay it visted array because for every node diiferent time..
+        // we will ge  less than maxmium time for viaiating eevry node value minmnmum cost
 
         // Min-heap: {cost, time, city}
-        priority_queue<vector<int>, vector<vector<int>>, greater<>> pq;
+    struct cmp {
+    bool operator()(const array<int,3>& a, const array<int,3>& b) const {
+        if (a[0] != b[0]) return a[0] > b[0]; // cost
+        return a[1] > b[1];                  // time
+    }
+};
 
+priority_queue<array<int,3>, vector<array<int,3>>, cmp> pq;
+
+        // i need to sort on basis of cost ,if cost eqaul hten min time
+
+        dp[0][0] = passingFees[0];
         pq.push({passingFees[0], 0, 0});
-        minTime[0] = 0;
 
         while (!pq.empty()) {
-            auto cur = pq.top();
-            pq.pop();
+            auto cur = pq.top(); pq.pop();
             int cost = cur[0];
             int time = cur[1];
             int city = cur[2];
 
-            // If we reach the destination, it's the minimum cost because of the Min-Heap
-            if (city == n - 1) return cost;
+            // If already worse, skip
+            if (cost > dp[city][time]) continue;
 
-            for (auto& edge : adj[city]) {
+            for (auto &edge : adj[city]) {
                 int nextCity = edge.first;
                 int travelTime = edge.second;
-                int newTime = time + travelTime;
 
-                if (newTime <= maxTime) {
-                    // PRUNING: Only push to PQ if this path offers a better time 
-                    // than any other path with a cost we've seen so far.
-                    if (newTime < minTime[nextCity]) {
-                        minTime[nextCity] = newTime;
-                        pq.push({cost + passingFees[nextCity], newTime, nextCity});
-                    }
+                int newTime = time + travelTime;
+                if (newTime > maxTime) continue;
+
+                int newCost = cost + passingFees[nextCity];
+
+                if (newCost < dp[nextCity][newTime]) {
+                    dp[nextCity][newTime] = newCost;
+                    pq.push({newCost, newTime, nextCity});
                 }
             }
         }
 
-        return -1;
+        int ans = INF;
+        // like we dont know what time so we iterate 0 to maxtime allwed and chekc in dp is minimum cost
+        for (int t = 0; t <= maxTime; t++) {
+            ans = min(ans, dp[n - 1][t]);
+        }
+
+        return ans == INF ? -1 : ans;
     }
 };
