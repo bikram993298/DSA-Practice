39e6ea81c9461671c3ab182dcca1f7371c0a0aab
sparrow <150315437+bikram993298@users.noreply.github.com>
2026-02-05 20:34:27 +0530

    Time: 11 ms (74.86%), Space: 40.8 MB (5.8%) - LeetHub

--- 1292-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/1292-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold.cpp
+++ 1292-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/1292-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold.cpp
@@ -1,40 +1,93 @@
+// class Solution {
+// public:
+//     int maxSideLength(vector<vector<int>>& mat, int threshold) {
+//         int m=mat.size();
+//         int n=mat[0].size();
+
+//         vector<vector<int>> pref(m + 1,vector<int>(n+1, 0));
+
+        
+//         for (int i=1; i<=m;i++) {
+//             for (int j=1; j<=n; j++) {
+//                 pref[i][j] = mat[i-1][j-1] + pref[i-1][j]+ pref[i][j -1]- pref[i-1][j-1];
+//             }
+//         }
+
+//         int ans=0;
+
+//         // there is a cathc htht o c hec k all possible square staring from thaht posiiton you have to create offser of isze
+//         for (int len=1; len<=min(m,n);len++) {
+//             for (int i=0;i+len<=m;i++) {
+//                 for (int j=0;j+len<=n;j++) {
+
+//                     int sum = pref[i+len][j+len]
+//                             - pref[i][j+len]
+//                             - pref[i+len][j]
+//                             + pref[i][j];
+
+//                     if (sum<=threshold) {
+//                         ans=len;
+//                     }
+//                 }
+//             }
+//         }
+
+//         return ans;
+//     }
+// };
+
+
+// // O(m * n * min(m, n))
+
+
 class Solution {
 public:
     int maxSideLength(vector<vector<int>>& mat, int threshold) {
-        int m=mat.size();
-        int n=mat[0].size();
+        int m = mat.size();
+        int n = mat[0].size();
 
-        vector<vector<int>> pref(m + 1,vector<int>(n+1, 0));
+        // prefix sum array
+        vector<vector<int>> pref(m + 1, vector<int>(n + 1, 0));
 
-        
-        for (int i=1; i<=m;i++) {
-            for (int j=1; j<=n; j++) {
-                pref[i][j] = mat[i-1][j-1] + pref[i-1][j]+ pref[i][j -1]- pref[i-1][j-1];
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                pref[i][j] = mat[i - 1][j - 1]
+                           + pref[i - 1][j]
+                           + pref[i][j - 1]
+                           - pref[i - 1][j - 1];
             }
         }
 
-        int ans=0;
+        int low = 0, high = min(m, n);
+        int ans = 0;
 
-        // there is a cathc htht o c hec k all possible square staring from thaht posiiton you have to create offser of isze
-        for (int len=1; len<=min(m,n);len++) {
-            for (int i=0;i+len<=m;i++) {
-                for (int j=0;j+len<=n;j++) {
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            bool ok = false;
 
-                    int sum = pref[i+len][j+len]
-                            - pref[i][j+len]
-                            - pref[i+len][j]
+            for (int i = 0; i + mid <= m; i++) {
+                for (int j = 0; j + mid <= n; j++) {
+                    int sum = pref[i + mid][j + mid]
+                            - pref[i][j + mid]
+                            - pref[i + mid][j]
                             + pref[i][j];
 
-                    if (sum<=threshold) {
-                        ans=len;
+                    if (sum <= threshold) {
+                        ok = true;
+                        break;
                     }
                 }
+                if (ok) break;
+            }
+
+            if (ok) {
+                ans = mid;
+                low = mid + 1;
+            } else {
+                high = mid - 1;
             }
         }
 
         return ans;
     }
 };
-
-
-// O(m * n * min(m, n))
