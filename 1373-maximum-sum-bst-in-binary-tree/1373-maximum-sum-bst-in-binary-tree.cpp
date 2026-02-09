9852bd13d902a1176c0b2777bbcbeaa0324b8dd9
sparrow <150315437+bikram993298@users.noreply.github.com>
2026-02-09 12:04:06 +0530

    Time: 731 ms (20.12%), Space: 249.9 MB (6.58%) - LeetHub

--- 1373-maximum-sum-bst-in-binary-tree/1373-maximum-sum-bst-in-binary-tree.cpp
+++ 1373-maximum-sum-bst-in-binary-tree/1373-maximum-sum-bst-in-binary-tree.cpp
@@ -1,46 +1,38 @@
 class Solution {
 public:
-
     long long ans = 0;
 
-    bool dfs(TreeNode* root,
-             long long &sum,
-             int &mini,
-             int &maxi) {
+    vector<long long> dfs(TreeNode* root) {
 
-        if (!root) {
-            sum = 0;
-            mini = INT_MAX;
-            maxi = INT_MIN;
-            return true;
+        // {min, max, sum}
+        if (!root)
+            return {(long long)INT_MAX, (long long)INT_MIN, 0};
+
+        auto left = dfs(root->left);
+        auto right = dfs(root->right);
+
+        // Check BST condition
+        if (root->val > left[1] && root->val < right[0]) {
+
+            long long currsum = left[2] + right[2] + root->val;
+
+            ans = max(ans, currsum);
+
+            long long minval = min((long long)root->val, left[0]);
+            long long maxval = max((long long)root->val, right[1]);
+
+            return {minval, maxval, currsum};
         }
 
-        long long lsum, rsum;
-        int lmin, lmax, rmin, rmax;
-
-        bool lb = dfs(root->left, lsum, lmin, lmax);
-        bool rb = dfs(root->right, rsum, rmin, rmax);
-
-        if (lb && rb && lmax < root->val && root->val < rmin) {
-
-            sum = lsum + rsum + root->val;
-            mini = min(root->val, lmin);
-            maxi = max(root->val, rmax);
-
-            ans = max(ans, sum);
-
-            return true;
-        }
-
-        return false;
+        // Not BST
+        long long maxsum = max(left[2], right[2]);
+        return {(long long)INT_MIN, (long long)INT_MAX, maxsum};
     }
 
     int maxSumBST(TreeNode* root) {
 
-        long long sum;
-        int mini, maxi;
-
-        dfs(root, sum, mini, maxi);
+        ans = 0;
+        dfs(root);
 
         return ans;
     }
