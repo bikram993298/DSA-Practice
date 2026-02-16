091f21b8852d817d22d3dc4f39aad45e2b8fa2a6
sparrow <150315437+bikram993298@users.noreply.github.com>
2026-02-16 19:35:13 +0530

    Time: 711 ms (70.57%), Space: 564.7 MB (6.76%) - LeetHub

--- 3836-maximum-score-using-exactly-k-pairs/3836-maximum-score-using-exactly-k-pairs.cpp
+++ 3836-maximum-score-using-exactly-k-pairs/3836-maximum-score-using-exactly-k-pairs.cpp
@@ -1,3 +1,114 @@
+// // class Solution {
+// //   vector<vector<vector<long long>>> dp;
+
+
+
+
+
+// // public:
+
+
+
+// //        long long solve(int i,int j,vector<int>& nums1,vector<int> & nums2,int k){
+// //         // base case
+
+// //         if(k==0){
+// //             return 0;
+// //         }
+// //         if(i>=nums1.size() || j>=nums2.size()) return LLONG_MIN;
+
+
+
+
+// //         // int take=0;
+
+// //        long long take = LLONG_MIN;
+
+// //      for(int m=j;m<nums2.size();m++){
+
+// //         long long next=solve(i+1,m+1,nums1,nums2,k-1);
+// //         if(next!=LLONG_MIN){
+// //              take = max(take,1LL * nums1[i] * nums2[m] +solve(i+1,m+1,nums1,nums2,k-1)
+// //     );
+
+// //         }
+   
+// // }
+
+// //         long long nottake=solve(i+1,j,nums1,nums2,k);
+
+
+
+
+// //         return max(take,nottake);
+
+
+
+
+// //        }
+// //     long long maxScore(vector<int>& nums1, vector<int>& nums2, int k) {
+
+// //       dp.resize(nums1.size(),vector<int>(nums2.size(),))
+// //         long long ans=solve(0,0,nums1,nums2,k);
+// //         return ans;
+        
+// //     }
+// // };
+// class Solution {
+
+//     vector<vector<vector<long long>>> dp;
+
+//     const long long NEG = LLONG_MIN / 2;
+
+// public:
+
+//     long long solve(int i,int j,
+//                     vector<int>& nums1,
+//                     vector<int>& nums2,
+//                     int k){
+
+//         if(k == 0)
+//             return 0;
+
+//         if(i >= nums1.size() || j >= nums2.size())
+//             return NEG;
+
+//         if(dp[i][j][k] != NEG)
+//             return dp[i][j][k];
+
+//         long long take = NEG;
+
+//         for(int m=j; m<nums2.size(); m++){
+
+//             long long next =
+//                 solve(i+1,m+1,nums1,nums2,k-1);
+
+//             if(next != NEG)
+//                 take = max(take,
+//                     1LL * nums1[i] * nums2[m] + next);
+//         }
+
+//         long long nottake =
+//             solve(i+1,j,nums1,nums2,k);
+
+//         return dp[i][j][k] = max(take,nottake);
+//     }
+
+//     long long maxScore(vector<int>& nums1,
+//                        vector<int>& nums2,
+//                        int k){
+
+//         int n = nums1.size();
+//         int m = nums2.size();
+
+//         dp.assign(n,
+//             vector<vector<long long>>(m,
+//                 vector<long long>(k+1, NEG)));
+
+//         return solve(0,0,nums1,nums2,k);
+//     }
+// };
+// o(n3*k) time limit exceed
 class Solution {
 public:
 
