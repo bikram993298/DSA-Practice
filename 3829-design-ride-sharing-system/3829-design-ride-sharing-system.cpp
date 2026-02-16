8cfd382806829e75351c10ebd1f817ee4afd319f
sparrow <150315437+bikram993298@users.noreply.github.com>
2026-02-16 23:53:45 +0530

    Time: 45 ms (63.36%), Space: 144.8 MB (68.58%) - LeetHub

---
+++ 3829-design-ride-sharing-system/3829-design-ride-sharing-system.cpp
@@ -0,0 +1,59 @@
+class RideSharingSystem {
+public:
+ queue<int> rider,driver; 
+ set<int> cancel;
+    RideSharingSystem() {
+       
+        
+    }
+    
+    void addRider(int riderId) {
+        rider.push(riderId);
+        if( cancel.count(riderId)){
+            cancel.erase(riderId);
+        }
+        
+    }
+    
+    void addDriver(int driverId) {
+        driver.push(driverId);
+        
+    }
+    
+    vector<int> matchDriverWithRider() {
+        if(rider.empty() || driver.empty()) return {-1,-1};
+
+        int rtop=rider.front();
+        while(cancel.count(rtop)){
+            rider.pop();
+            rtop=rider.front();
+        }
+        if(rider.empty()) return {-1,-1};
+        rider.pop();
+        int dtop=driver.front();
+        driver.pop();
+
+        return {dtop,rtop};
+
+
+        
+    }
+    
+    void cancelRider(int riderId) {
+
+
+
+        cancel.insert(riderId);
+
+        
+    }
+};
+
+/**
+ * Your RideSharingSystem object will be instantiated and called as such:
+ * RideSharingSystem* obj = new RideSharingSystem();
+ * obj->addRider(riderId);
+ * obj->addDriver(driverId);
+ * vector<int> param_3 = obj->matchDriverWithRider();
+ * obj->cancelRider(riderId);
+ */
