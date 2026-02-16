4890cd2ae30fb9f668864723e2bf867504f05223
sparrow <150315437+bikram993298@users.noreply.github.com>
2026-02-16 19:58:31 +0530

    Time: 54 ms (70.34%), Space: 185.8 MB (91.16%) - LeetHub

--- 3835-count-subarrays-with-cost-less-than-or-equal-to-k/3835-count-subarrays-with-cost-less-than-or-equal-to-k.cpp
+++ 3835-count-subarrays-with-cost-less-than-or-equal-to-k/3835-count-subarrays-with-cost-less-than-or-equal-to-k.cpp
@@ -6,6 +6,12 @@ public:
         int n=nums.size();
         long long total=1LL*n*(n+1)/2;
         // our idea is to calclute no of subtring that less than k
+
+
+
+
+        // the main idea is to count invalid because it clicks in my mind if i got valid subarray then right part of it like i to j is valid thn i to j+1,j+2 till n will be valid because i-j+1 will increase max may incarease but min will may be soame,,so monotinic increaseing so we are count subarray gretaer than k then minize it form totoal
+        // another in deque logic we top gives valid max or min,,so whenever get greter value for max que pop and push alogh with ,,if less eelmnt then push ,,play woth index not number,,
         int l=0;
         long long cnt=0;
         for(int i=0;i<n;i++){
@@ -21,6 +27,7 @@ public:
 
           while(l<=i &&(long long)(i-l+1)*(nums[maxi.front()]-nums[mini.front()])>k){
             cnt+=n-i;
+            // during shrinking phase it may happen in deques we have track elemnt so l index should be trackec maxi or mini front that in contrivutuin if it comes ir euqla to l that we are decrementing then pop,,it will give bext biggest elemnt,,in deque thus works
             if(maxi.front() == l)
                     maxi.pop_front();
 
