fa099f44080f99fb319ff5ddeb52d3c6123e0040
sparrow <150315437+bikram993298@users.noreply.github.com>
2026-02-17 10:39:33 +0530

    Time: 0 ms (100%), Space: 9.2 MB (50.09%) - LeetHub

--- 3823-reverse-letters-then-special-characters-in-a-string/3823-reverse-letters-then-special-characters-in-a-string.cpp
+++ 3823-reverse-letters-then-special-characters-in-a-string/3823-reverse-letters-then-special-characters-in-a-string.cpp
@@ -1,61 +1,71 @@
 class Solution {
 public:
     string reverseByType(string s) {
-    //     int n=s.size();
-    //     string lower="";
-    //     string special="";
-    //     vector<bool> lower_pre(n,0);
+        int n=s.size();
+        string lower="";
+        string special="";
+        vector<bool> lower_pre(n,0);
 
-    //     for(int i=0;i<n;i++){
-    //         if(islower(s[i])){
-    //             lower+=s[i];
-    //             lower_pre[i]=true;
-    //         }else{
-    //             special+=s[i];
-    //         }
-    //     }
+        for(int i=0;i<n;i++){
+            if(islower(s[i])){
+                lower+=s[i];
+                lower_pre[i]=true;
+            }else{
+                special+=s[i];
+            }
+        }
 
-    //     reverse(lower.begin(),lower.end());
-    //     reverse(special.begin().special.emd());
+        reverse(lower.begin(),lower.end());
+        reverse(special.begin(),special.end());
 
-    //    string ans="";
-    //     for(int i=0;i<n;i++){
+       string ans="";
+       int l=0;
+       int r=0;
+        for(int i=0;i<n;i++){
+            if(lower_pre[i]){
+                ans+=lower[l];
+                l++;
+
+            }else{
+                ans+=special[r];
+                r++;
+            }
             
 
-    //     }/
-
-       int l=0;
-       int n=s.size();
-       int r=n-1;
-       while(l<r){
-        if(islower(s[l]) && islower(s[r])){
-            swap(s[l],s[r]);
-            l++;
-            r--;
-        }else if(islower(s[l])){
-            r--;
-        }else{
-            l++;
         }
-       }
+         return ans;
+    //    int l=0;
+    //    int n=s.size();
+    //    int r=n-1;
+    //    while(l<r){
+    //     if(islower(s[l]) && islower(s[r])){
+    //         swap(s[l],s[r]);
+    //         l++;
+    //         r--;
+    //     }else if(islower(s[l])){
+    //         r--;
+    //     }else{
+    //         l++;
+    //     }
+    //    }
 
-       l=0;
-        r=n-1;
+    //    l=0;
+    //     r=n-1;
 
-        while(l<r){
-             if(!islower(s[l]) && !islower(s[r])){
-            swap(s[l],s[r]);
-            l++;
-            r--;
-        }else if(!islower(s[l])){
-            r--;
-        }else{
-            l++;
-        }
+    //     while(l<r){
+    //          if(!islower(s[l]) && !islower(s[r])){
+    //         swap(s[l],s[r]);
+    //         l++;
+    //         r--;
+    //     }else if(!islower(s[l])){
+    //         r--;
+    //     }else{
+    //         l++;
+    //     }
 
-        }
+    //     }
 
-        return s;
+    //     return s;
         
     }
 };
