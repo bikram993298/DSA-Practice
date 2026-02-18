c9fcafbf9cfbd4bbfcc25de836b164026bbbe478
sparrow <150315437+bikram993298@users.noreply.github.com>
2026-02-19 01:20:34 +0530

    Time: 188 ms (89.82%), Space: 89.2 MB (13.83%) - LeetHub

--- 3804-number-of-centered-subarrays/3804-number-of-centered-subarrays.cpp
+++ 3804-number-of-centered-subarrays/3804-number-of-centered-subarrays.cpp
@@ -10,7 +10,7 @@ public:
         vector<int > prefix(n + 1,0);
 
     // prefix[0] = 0;
-
+// 1 based prefissum
   for(int i = 1; i <= n; i++) {
     prefix[i] = prefix[i-1] + nums[i-1];
 }
@@ -29,3 +29,46 @@ public:
         return cnt;
     }
 };
+
+
+
+
+
+// Why this is faster
+
+// Your version:
+
+// set insert = log n
+// Total = O(n² log n)
+
+
+// This version:
+
+// unordered_set insert = O(1)
+// Total = O(n²)
+// class Solution {
+// public:
+//     int centeredSubarrays(vector<int>& nums) {
+
+//         int n = nums.size();
+//         int cnt = 0;
+
+//         for(int i = 0; i < n; i++) {
+
+//             long long sum = 0;
+//             unordered_set<long long> st;
+
+//             for(int j = i; j < n; j++) {
+
+//                 sum += nums[j];
+
+//                 st.insert(nums[j]);
+
+//                 if(st.count(sum))
+//                     cnt++;
+//             }
+//         }
+
+//         return cnt;
+//     }
+// };
