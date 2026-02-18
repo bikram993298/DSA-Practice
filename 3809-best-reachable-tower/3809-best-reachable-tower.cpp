966cb7d9e0cd3e4bac2496775a131c7b090413e7
sparrow <150315437+bikram993298@users.noreply.github.com>
2026-02-18 18:41:53 +0530

    Time: 22 ms (43.85%), Space: 284.6 MB (32.43%) - LeetHub

---
+++ 3809-best-reachable-tower/3809-best-reachable-tower.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    vector<int> bestTower(vector<vector<int>>& towers,
+                          vector<int>& center,
+                          int radius) {
+
+        // max heap based on quality,
+        // if tie → smaller x, then smaller y
+        priority_queue<
+            pair<int, pair<int,int>>
+        > pq;
+
+        for(int i = 0; i < towers.size(); i++) {
+
+            int x = towers[i][0];
+            int y = towers[i][1];
+            int q = towers[i][2];
+
+            int dist = abs(x - center[0]) +
+                       abs(y - center[1]);
+
+            if(dist <= radius) {
+
+                // push quality,
+                // and negative coordinates for lexicographic min
+                pq.push({q, {-x, -y}});
+            }
+        }
+
+        if(pq.empty())
+            return {-1, -1};
+
+        auto top = pq.top();
+
+        return {-top.second.first,
+                -top.second.second};
+    }
+};
