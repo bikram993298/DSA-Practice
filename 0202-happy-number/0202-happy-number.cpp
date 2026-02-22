26e3f755bea4455cdb7f32290803db5b8316fe30
sparrow <150315437+bikram993298@users.noreply.github.com>
2026-02-22 13:32:47 +0530

    Time: 2 ms (23.47%), Space: 8.4 MB (34.54%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    
+    int getNext(int n) {
+        int sum = 0;
+        while(n > 0) {
+            int d = n % 10;
+            sum += d * d;
+            n /= 10;
+        }
+        return sum;
+    }
+    
+    bool isHappy(int n) {
+        
+        unordered_set<int> seen;
+        
+        while(n != 1 && seen.find(n) == seen.end()) {
+            
+            seen.insert(n);
+            n = getNext(n);
+        }
+        
+        return n == 1;
+    }
+};
+
+
+// n = 2
+
+// 2² = 4
+// 4² = 16
+// 1²+6²=37
+// 3²+7²=58
+// 5²+8²=89
+// 8²+9²=145
+// 1²+4²+5²=42
+// 4²+2²=20
+// 2²+0²=4 → cycle starts
