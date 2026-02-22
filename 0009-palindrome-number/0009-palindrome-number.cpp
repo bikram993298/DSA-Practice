978e93a4dafecb1c556b0803f8b718db354830a9
sparrow <150315437+bikram993298@users.noreply.github.com>
2026-02-22 12:23:17 +0530

    Time: 0 ms (100%), Space: 8.6 MB (38.09%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,20 +1,38 @@
+// class Solution {
+// public:
+//     bool isPalindrome(int x) {
+        
+//         string s = to_string(x);
+        
+//         int left = 0;
+//         int right = s.size() - 1;
+        
+//         while(left < right) {
+//             if(s[left] != s[right])
+//                 return false;
+            
+//             left++;
+//             right--;
+//         }
+        
+//         return true;
+//     }
+// };
+//reverse the number then check 
 class Solution {
 public:
     bool isPalindrome(int x) {
         
-        string s = to_string(x);
+        if(x < 0) return false;
         
-        int left = 0;
-        int right = s.size() - 1;
+        int original = x;
+        long long reverse = 0;
         
-        while(left < right) {
-            if(s[left] != s[right])
-                return false;
-            
-            left++;
-            right--;
+        while(x > 0) {
+            reverse = reverse * 10 + x % 10;
+            x /= 10;
         }
         
-        return true;
+        return original == reverse;
     }
 };
