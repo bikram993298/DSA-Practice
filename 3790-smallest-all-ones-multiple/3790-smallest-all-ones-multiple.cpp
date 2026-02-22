9de841a9c1b8527e990d0721bd2eef5802cc4293
sparrow <150315437+bikram993298@users.noreply.github.com>
2026-02-22 11:52:52 +0530

    Time: 51 ms (25.03%), Space: 8.6 MB (58.02%) - LeetHub

--- 3790-smallest-all-ones-multiple/3790-smallest-all-ones-multiple.cpp
+++ 3790-smallest-all-ones-multiple/3790-smallest-all-ones-multiple.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
     int minAllOneMultiple(int k) {
-          int rem = 0;
-        
+          int num = 0;
+        // if u are thinking to make numer one by one till k then you can store it in long long int because  2 <= k <= 105 ,,only if k is 9 means upto value,,10^9 we can store,,means 9 digit not 10^5 digits
         for(int len = 1; len <= k; len++) {
+            // so we are using modulo proprery
             
-            rem = (rem * 10 + 1) % k;
+            num= (num * 10 + 1) % k;
             
-            if(rem == 0)
+            if(num == 0)
                 return len;
         }
         
