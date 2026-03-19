073e98a3142f899a70c135d08ce8e41ff5c8120a
sparrow <150315437+bikram993298@users.noreply.github.com>
2026-03-19 11:50:55 +0530

    Time: 7 ms (40%), Space: 17.5 MB (41.82%) - LeetHub

--- 1573-number-of-ways-to-split-a-string/1573-number-of-ways-to-split-a-string.cpp
+++ 1573-number-of-ways-to-split-a-string/1573-number-of-ways-to-split-a-string.cpp
@@ -1,60 +1,81 @@
 class Solution {
+
 public:
+//    int solve(string s,int i,int k){
+
+//     // base cond
+
+
+
+//    }
+
+
+   
     int numWays(string s) {
 
-        const long long MOD = 1e9 + 7;
+        // int ans=solve(s,0,3);
 
-        long long total_1 = 0;
-        for(int i = 0; i < s.size(); i++){
-            if(s[i] == '1') total_1++;
+        // return ans;
+
+   const long long MOD=1e9+7;
+
+
+        int total_1=0;
+        for(int i=0;i<s.size();i++){
+
+            if(s[i]=='1')total_1++;
         }
-
-        if(total_1 % 3 != 0) return 0;
-
-        if(total_1 == 0){
+         if(total_1 == 0){
             long long n = s.size();
             return ((n-1)*(n-2)/2) % MOD;
         }
 
-        total_1 = total_1 / 3;
 
-        int cnt = 0;
-        int pos1 = 0;
-
-        for(int i = 0; i < s.size(); i++){
-            if(s[i] == '1') cnt++;
-            if(cnt == total_1){
-                pos1 = i;
+        if(total_1%3!=0) return 0;
+        total_1=total_1/3;
+        int cnt=0;
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='1') cnt++;
+            if(cnt==total_1){
+                cnt=i;
                 break;
             }
         }
-
-        int cnt_0 = 0;
-        int i = pos1 + 1;
-        while(i < s.size() && s[i] == '0'){
+       int cnt_0=0;
+       cnt++;
+        while(cnt<s.size()&&s[cnt]=='0'){
+          cnt++; 
             cnt_0++;
-            i++;
         }
-
-        cnt = 0;
-        int pos2 = 0;
-
-      
-        for(int i = s.size()-1; i >= 0; i--){
-            if(s[i] == '1') cnt++;
-            if(cnt == total_1){
-                pos2 = i;
+         cnt=0;
+        
+        for(int i=s.size()-1;i>=0;i--){
+            if(s[i]=='1') cnt++;
+            if(cnt==total_1){
+                cnt=i;
                 break;
             }
+
+
+
         }
 
-        int cnt_0l = 0;
-        i = pos2 - 1;
-        while(i >= 0 && s[i] == '0'){
+
+
+         int cnt_0l=0;
+         cnt--;
+        while(cnt>=0&&s[cnt]=='0'){
+            // if(s[cnt++]=='1'){
+            //     break;
+
+            // }
+            cnt--;
             cnt_0l++;
-            i--;
         }
 
-        return ((long long)(cnt_0 + 1) * (cnt_0l + 1)) % MOD;
+
+
+        return ((long long)(cnt_0+1)*(1+cnt_0l))%MOD;
     }
 };
+//111           0101010         111
