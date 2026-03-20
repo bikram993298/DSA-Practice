0c33665d979535c74a0098aa890f7db6e2e1a644
sparrow <150315437+bikram993298@users.noreply.github.com>
2026-03-21 02:41:27 +0530

    Time: 1 ms (68.02%), Space: 117.9 MB (82.18%) - LeetHub

---
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -0,0 +1,60 @@
+// class Solution {
+// public:
+//     bool isPalindrome(ListNode* head) {
+//         vector<int> arr;
+        
+//         while(head) {
+//             arr.push_back(head->val);
+//             head = head->next;
+//         }
+        
+//         int l = 0, r = arr.size() - 1;
+//         while(l < r) {
+//             if(arr[l] != arr[r]) return false;
+//             l++; r--;
+//         }
+        
+//         return true;
+//     }
+// };
+class Solution {
+public:
+    ListNode* reverse(ListNode* head) {
+        ListNode* prev = NULL;
+        
+        while(head) {
+            ListNode* next = head->next;
+            head->next = prev;
+            prev = head;
+            head = next;
+        }
+        
+        return prev;
+    }
+    
+    bool isPalindrome(ListNode* head) {
+        if(!head || !head->next) return true;
+        
+        //find middle
+        ListNode* slow = head;
+        ListNode* fast = head;
+        
+        while(fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        
+     
+        ListNode* second = reverse(slow);
+        ListNode* first = head;
+        
+     
+        while(second) {
+            if(first->val != second->val) return false;
+            first = first->next;
+            second = second->next;
+        }
+        
+        return true;
+    }
+};
