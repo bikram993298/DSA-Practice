a4d8512ec0eec9c4e96fe98fa8c116e1dc473b9b
sparrow <150315437+bikram993298@users.noreply.github.com>
2026-03-20 12:47:44 +0530

    Time: 0 ms (100%), Space: 14.7 MB (52.31%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,25 +1,55 @@
+// class Solution {
+// public:
+//     vector<int> twoSum(vector<int>& nums, int target) {
+//         map<int,int>mp;
+//         mp[0]=1;
+//         int sum=0;
+//         vector<int> anss;
+//         // for(int i=0;i<nums.size();i++){
+//         //     sum=sum+nums[i];
+//         //     mp[sum]++;
+//         // }
+//         // for(int i=0;i<nums.size();i++){
+//         //     mp[sum-]
+//         // }
+//         // sumi freqi
+//         // sumi indi
+//         // 1 2 3 3 
+//         // 1 0
+//         // 2 1
+//         // 3 {2 3}
+//         // map<int,vector<int>>
+//         for (int i = 0; i < nums.size(); i++) {
+//             mp[nums[i]] = i;
+//         }
+//         // 2 0
+//         // 7 1
+//         //,,
+
+//         for (int i = 0; i < nums.size(); i++) {
+//             if (mp.find(target - nums[i]) != mp.end()&&mp[target-nums[i]]!=i) {
+//                 anss.push_back(i);
+//                 anss.push_back(mp[target-nums[i]]);
+//                 break;
+//             }
+//         }
+//         return anss;
+//     }
+// };
+
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        map<int, int> mp;  // To store number and its index
-        vector<int> ans;
+        unordered_map<int, int> pairIdx;
 
-        // Store all elements and their indices in the map
-        for (int i = 0; i < nums.size(); i++) {
-            mp[nums[i]] = i;
-        }
-
-        // Find the two indices
-        for (int i = 0; i < nums.size(); i++) {
-            int complement = target - nums[i];
-            // Check if complement exists and is not the same index
-            if (mp.find(complement) != mp.end() && mp[complement] != i) {
-                ans.push_back(i);
-                ans.push_back(mp[complement]);
-                break;  // Return immediately after finding the pair
+        for (int i = 0; i < nums.size(); ++i) {
+            int num = nums[i];
+            if (pairIdx.find(target - num) != pairIdx.end()) {
+                return {i, pairIdx[target - num]};
             }
+            pairIdx[num] = i;
         }
 
-        return ans;
+        return {};        
     }
 };
