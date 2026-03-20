3d33cac6e003d148f38e3b4de99e43e7548cc96c
sparrow <150315437+bikram993298@users.noreply.github.com>
2026-03-20 20:51:02 +0530

    Time: 0 ms (100%), Space: 11 MB (51.76%) - LeetHub

--- 2395-find-subarrays-with-equal-sum/2395-find-subarrays-with-equal-sum.cpp
+++ 2395-find-subarrays-with-equal-sum/2395-find-subarrays-with-equal-sum.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool findSubarrays(vector<int>& nums) {
         unordered_set<int> st;
-        
+        // so we are storing in terms of coniguis two ,,and finding in map is there exist somethin like two sum
         for(int i = 0; i < nums.size() - 1; i++) {
             int sum = nums[i] + nums[i+1];
             
