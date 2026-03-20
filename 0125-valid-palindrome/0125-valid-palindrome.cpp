a6ab19599c3ef8d3e3b74b5724110707aba37afd
sparrow <150315437+bikram993298@users.noreply.github.com>
2026-03-21 02:49:31 +0530

    Time: 0 ms (100%), Space: 10.7 MB (5.29%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+bool check(string s){
+    int i=0;
+    int j=s.size()-1;
+    while(i<j){
+        if(s[i]!=s[j]) return false;
+        i++;
+        j--;
+    }
+    return true;
+}
+    bool isPalindrome(string s) {
+        string ans="";
+
+         for(int i = 0; i < s.size(); i++){
+            if(s[i] >= 'A' && s[i] <= 'Z'){
+                ans += tolower(s[i]); 
+            } else if(s[i] >= 'a' && s[i] <= 'z'){
+                ans += s[i]; 
+            } else if(s[i] >= '0' && s[i] <= '9'){
+                ans += s[i]; 
+            }
+        }
+
+
+        return check(ans);
+        
+    }
+};
