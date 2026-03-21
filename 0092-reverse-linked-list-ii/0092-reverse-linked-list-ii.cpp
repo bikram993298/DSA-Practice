884149c227322c4239269afd75fb070bb0411dc5
sparrow <150315437+bikram993298@users.noreply.github.com>
2026-03-21 12:31:28 +0530

    Time: 0 ms (100%), Space: 11.3 MB (38.24%) - LeetHub

--- 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.cpp
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.cpp
@@ -18,6 +18,12 @@ public:
         
         for(int i = 0; i < right - left; i++) {
             ListNode* temp = curr->next;
+
+            //here what we are doing we are oushing into the front
+            // 12 3 4 5
+            // 1 3 2 4 5
+            // 1 4 3 2 5
+            // 1 5 4 3 2
             
             curr->next = temp->next;
             temp->next = prev->next;
@@ -27,3 +33,58 @@ public:
         return dummy->next;
     }
 };
+
+
+// class Solution {
+// public:
+
+//     ListNode* reverseList(ListNode* head) {
+//         ListNode* prev = NULL;
+//         ListNode* curr = head;
+
+//         while(curr) {
+//             ListNode* temp = curr->next;
+//             curr->next = prev;
+//             prev = curr;
+//             curr = temp;
+//         }
+//         return prev;
+//     }
+
+//     ListNode* reverseBetween(ListNode* head, int left, int right) {
+//         if(!head || left == right) return head;
+
+//         ListNode* dummy = new ListNode(0);
+//         dummy->next = head;
+
+//         ListNode* prev = dummy;
+
+//         // move to (left-1)
+//         for(int i = 1; i < left; i++) {
+//             prev = prev->next;
+//         }
+
+//         ListNode* start = prev->next;      // left node
+//         ListNode* end = start;
+
+//         // move to right
+//         for(int i = left; i < right; i++) {
+//             end = end->next;
+//         }
+
+//         ListNode* nextPart = end->next;
+
+//         // cut
+//         prev->next = NULL;
+//         end->next = NULL;
+
+//         // reverse
+//         ListNode* newHead = reverseList(start);
+
+//         // reconnect
+//         prev->next = newHead;
+//         start->next = nextPart;
+
+//         return dummy->next;
+//     }
+// };
