da1bce32939300bd41ef8f205f65069b62816c7d
sparrow <150315437+bikram993298@users.noreply.github.com>
2026-03-22 11:37:32 +0530

    Time: 3 ms (51.02%), Space: 24.1 MB (35.93%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -1,34 +1,46 @@
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        int n=board.size();
-        int m=board[0].size();
-        vector<int> row(9,0);
-        vector<int> col(9,0);
-        vector<int> box(9,0);
-
-
-
-
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                    if(board[i][j]=='.')continue;
-
-                int digit=board[i][j]-'1';
-              
-                int mask=1<<digit;
-                int index=(i / 3) * 3 + (j / 3);
-                //000010000
-               if ((row[i] & mask) || (col[j] & mask) || (box[index] & mask)) {
-                return false; // duplicate found
-            }
-                row[i]|=mask;
-                col[j]|=mask;
-                box[index]|=mask;
-
+        
+        // rows
+        for(int i = 0; i < 9; i++) {
+            unordered_set<char> st;
+            for(int j = 0; j < 9; j++) {
+                char c = board[i][j];
+                if(c == '.') continue;
+                if(st.count(c)) return false;
+                st.insert(c);
             }
         }
+        
+        // columns
+        for(int j = 0; j < 9; j++) {
+            unordered_set<char> st;
+            for(int i = 0; i < 9; i++) {
+                char c = board[i][j];
+                if(c == '.') continue;
+                if(st.count(c)) return false;
+                st.insert(c);
+            }
+        }
+        
+        // 3x3 boxes
+        for(int sr = 0; sr < 9; sr += 3) {
+            for(int sc = 0; sc < 9; sc += 3) {
+                
+                unordered_set<char> st;
+                
+                for(int i = sr; i < sr + 3; i++) {
+                    for(int j = sc; j < sc + 3; j++) {
+                        char c = board[i][j];
+                        if(c == '.') continue;
+                        if(st.count(c)) return false;
+                        st.insert(c);
+                    }
+                }
+            }
+        }
+        
         return true;
-
     }
 };
