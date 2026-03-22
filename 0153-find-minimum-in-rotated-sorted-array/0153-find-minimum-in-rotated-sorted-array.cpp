d141c3bde037929b566becc5338a040eecaa63f6
sparrow <150315437+bikram993298@users.noreply.github.com>
2026-03-22 10:57:49 +0530

    Time: 0 ms (100%), Space: 14.2 MB (49.45%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
@@ -5,15 +5,18 @@ public:
         
            int start=0;
         int end=nums.size()-1;
-       
+ // one of the important problem in binary serach hrere we are finidng only one element so while(l<r) used\
+ // always compare with right bcause right part ,, and alwasya discard the sorted part,,a nd end=mid,,remeber       
+     
         while(start<end){
              int mid=start+(end-start)/2;
            
-           if(nums[mid]<nums[end]){
-                end=mid;
+           if(nums[end]<nums[mid]){
+                start=mid+1;
             }
              else{
-                start=mid+1;
+              
+                end=mid;
             }
         }
         return nums[end];
