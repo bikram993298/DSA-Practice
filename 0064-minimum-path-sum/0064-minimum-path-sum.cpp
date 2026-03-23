e2a488bb1cd9be85e7416048c4eed43b2d9c95a4
sparrow <150315437+bikram993298@users.noreply.github.com>
2026-03-23 23:17:36 +0530

    Time: 0 ms (100%), Space: 16.3 MB (40.12%) - LeetHub

---
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+ vector<vector<int>> dp;
+
+    int solve(int i, int j, int m, int n,vector<vector<int>>& grid) {
+        if(i>=m || j>=n) return 1e9;
+        if(i == m-1 && j == n-1) return grid[i][j];
+        if(dp[i][j] != -1) return dp[i][j];
+
+        int result = INT_MAX;
+         result =min(result,grid[i][j]+solve(i+1, j, m, n,grid));
+        result =min(result,grid[i][j]+ solve(i, j+1, m, n,grid));
+
+        return dp[i][j] = result;
+    }
+
+    int minPathSum(vector<vector<int>>& grid) {
+         int m=grid.size();
+        int n=grid[0].size();
+       
+          dp.assign(m, vector<int>(n, -1));
+        return solve(0, 0, m, n,grid);
+        
+        
+    }
+};
+//if there is negactive elemnt then ans can be -1 so you dont use dp assign -1 
+// class Solution {
+// public:
+//     vector<vector<int>> dp;
+
+//     int solve(int i, int j, int m, int n, vector<vector<int>>& grid) {
+//         if(i == m-1 && j == n-1) return grid[i][j];
+//         if(dp[i][j] != INT_MAX) return dp[i][j];
+
+//         int result = INT_MAX;
+//         if(i+1 < m) result = min(result, grid[i][j] + solve(i+1, j, m, n, grid));
+//         if(j+1 < n) result = min(result, grid[i][j] + solve(i, j+1, m, n, grid));
+
+//         return dp[i][j] = result;
+//     }
+
+//     int minPathSum(vector<vector<int>>& grid) {
+//         int m = grid.size();
+//         int n = grid[0].size();
+//         dp.assign(m, vector<int>(n, INT_MAX));
+//         return solve(0, 0, m, n, grid);
+//     }
+// };
