34f00e65dac05323a2369861aed771cb603fd238
sparrow <150315437+bikram993298@users.noreply.github.com>
2026-03-23 23:40:24 +0530

    Time: 74 ms (35.77%), Space: 82.5 MB (34.31%) - LeetHub

---
+++ 0077-combinations/0077-combinations.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<vector<int>> result;
+    
+    void solve(int start, int n, int k, vector<int>& temp) {
+        // base case\t
+       if(k==0){
+        result.push_back(temp);
+        return;
+       }
+
+
+
+        for(int i=start;i<=n;i++){
+            temp.push_back(i);
+            solve(i+1,n,k-1,temp);
+            temp.pop_back();
+
+        }
+
+
+
+        return ;
+       
+    }
+    
+    vector<vector<int>> combine(int n, int k) {
+        vector<int> temp;
+        
+        solve(1, n, k, temp);
+        
+        return result;
+    }
+};
