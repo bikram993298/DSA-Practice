739e93fa93baf97877febcfb8418a974f45dc9c2
sparrow <150315437+bikram993298@users.noreply.github.com>
2026-03-23 23:04:46 +0530

    Time: 0 ms (100%), Space: 9.4 MB (75.19%) - LeetHub

--- 0059-spiral-matrix-ii/0059-spiral-matrix-ii.cpp
+++ 0059-spiral-matrix-ii/0059-spiral-matrix-ii.cpp
@@ -1,38 +1,61 @@
 class Solution {
 public:
     vector<vector<int>> generateMatrix(int n) {
-        if(n == 0) return {};
+        if(n == 0)
+            return {};
+        int m = n;
+        vector<vector<int>> matrix(m, vector<int>(n));
+        int top   = 0;
+        int down  = m-1;
+        int left  = 0;
+        int right = n-1;
         
-        vector<vector<int>> matrix(n, vector<int>(n));
-        
-        int top = 0, down = n-1;
-        int left = 0, right = n-1;
+       
+        //id
+        //0   -> left  to right
+        //1   -> top   to down
+        //2   -> right to left
+        //3   -> down  to top
         int counter = 1;
-        
         while(top <= down && left <= right) {
+            //left to right
             
-            for(int i = left; i <= right; i++)
-                matrix[top][i] = counter++;
+                for(int i = left; i<=right; i++) {
+                    matrix[top][i] = counter++;
+                }
+                
             
-            for(int i = top+1; i <= down; i++)
-                matrix[i][right] = counter++;
             
-            if(top != down) {
-                for(int i = right-1; i >= left; i--)
+            //top to down
+         
+                for(int i = top+1; i<=down; i++) {
+                    matrix[i][right] = counter++;
+                }
+              
+            
+            
+            //right to left
+           
+                for(int i = right-1; i>=left; i--) {
+                    if(top==down) continue;
                     matrix[down][i] = counter++;
-            }
+                }
             
-            if(left != right) {
-                for(int i = down-1; i > top; i--)
+            
+            //down to top
+           
+                for(int i = down-1; i>top; i--) {
+                    if(left==right) continue;
                     matrix[i][left] = counter++;
-            }
-            
-            top++;
+                }
+          
+             top++;
             down--;
             left++;
             right--;
+            
+           
         }
-        
         return matrix;
     }
 };
