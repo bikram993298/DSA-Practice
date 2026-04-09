a4d4129acefdda17473e10aa4752b496bf9ac08d
sparrow <150315437+bikram993298@users.noreply.github.com>
2026-04-09 18:52:53 +0530

    Time: 47 ms (53.62%), Space: 7.8 MB (82.3%) - LeetHub

---
+++ 0526-beautiful-arrangement/0526-beautiful-arrangement.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int count = 0;
+
+    void backtrack(int pos, int n, vector<bool>& used) {
+        // all positions filled
+        if (pos > n) {
+            count++;
+            // return;
+        }
+
+        for (int num = 1; num <= n; num++) {
+            if (!used[num] && (num % pos == 0 || pos % num == 0)) {
+                used[num] = true;
+
+                backtrack(pos + 1, n, used);
+
+                used[num] = false; // backtrack
+            }
+        }
+    }
+
+    int countArrangement(int n) {
+        vector<bool> used(n + 1, false);
+        backtrack(1, n, used);
+        return count;
+    }
+};
