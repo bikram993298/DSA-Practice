cc9acded809368846bff4b03300ec710cf984f3b
sparrow <150315437+bikram993298@users.noreply.github.com>
2026-04-09 18:34:10 +0530

    Time: 2 ms (66.51%), Space: 25.3 MB (47.71%) - LeetHub

--- 0519-random-flip-matrix/0519-random-flip-matrix.cpp
+++ 0519-random-flip-matrix/0519-random-flip-matrix.cpp
@@ -21,7 +21,7 @@ public:
         } else {
             actualIndex = r;
         }
-
+// li ke initially 1 2 3 4 5 6,,now after random 3 delete,,so we will decreasereamaing and  map me 3 index me last value 6 dal do,,q  ki 3 to remove ho chuka he and remaning bhi ek kom so 6 ko ou position pe daldo
         // find last available value
         int lastIndex;
         if (mp.find(remaining - 1) != mp.end()) {
@@ -45,3 +45,44 @@ public:
         remaining = rows * cols;
     }
 };
+
+
+// class Solution {
+//     int rows, cols;
+//     int remaining;
+//     unordered_map<int, int> mp;
+
+// public:
+//     Solution(int m, int n) {
+//         rows = m;
+//         cols = n;
+//         remaining = m * n;
+
+//         // initialize full map (NOT OPTIMAL)
+//         for (int i = 0; i < remaining; i++) {
+//             mp[i] = i;
+//         }
+//     }
+
+//     vector<int> flip() {
+//         int r = rand() % remaining;
+
+//         int val = mp[r];
+
+//         // swap with last
+//         mp[r] = mp[remaining - 1];
+
+//         remaining--;
+
+//         return {val / cols, val % cols};
+//     }
+
+//     void reset() {
+//         mp.clear();
+//         remaining = rows * cols;
+
+//         for (int i = 0; i < remaining; i++) {
+//             mp[i] = i;
+//         }
+//     }
+// };
