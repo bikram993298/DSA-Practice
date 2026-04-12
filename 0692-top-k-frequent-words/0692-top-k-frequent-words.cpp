1ad19db4722c4642e07bb68e3c63b747365c5bef
sparrow <150315437+bikram993298@users.noreply.github.com>
2026-04-12 14:02:00 +0530

    Time: 0 ms (100%), Space: 18.3 MB (11.21%) - LeetHub

---
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    struct cmp {
+        bool operator()(pair<int,string>& a, pair<int,string>& b) {
+            if (a.first == b.first)
+                return a.second < b.second; // larger word should come first (worse)
+            return  b.first<a.first;       // smaller freq is worse
+        }
+    };
+
+    vector<string> topKFrequent(vector<string>& words, int k) {
+        unordered_map<string,int> mp;
+
+        for (auto w : words) {
+            mp[w]++;
+        }
+
+        priority_queue<pair<int,string>, vector<pair<int,string>>, cmp> pq;
+
+        for (auto it : mp) {
+            pq.push({it.second, it.first});
+
+            if (pq.size() > k) {
+                pq.pop(); // remove worst
+            }
+        }
+
+        vector<string> ans;
+
+        while (!pq.empty()) {
+            ans.push_back(pq.top().second);
+            pq.pop();
+        }
+
+        reverse(ans.begin(), ans.end());
+
+        return ans;
+    }
+};
