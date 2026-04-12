a1cddcd81a50a354bb08ac9f05c96a1c87726f6a
sparrow <150315437+bikram993298@users.noreply.github.com>
2026-04-12 15:13:53 +0530

    Time: 7 ms (26.9%), Space: 13.1 MB (22.52%) - LeetHub

--- 0696-count-binary-substrings/0696-count-binary-substrings.cpp
+++ 0696-count-binary-substrings/0696-count-binary-substrings.cpp
@@ -4,7 +4,7 @@ public:
         int prev = 0;
         int curr = 1;
         int ans = 0;
-
+// main idea msintain prev how many o or 1 ,then if you find different then min of prev and current will give u substringm,,do the last pre,count will be unmatched beacuse wghen u reach last for loop khtatam no else confition so add extra
         for (int i = 1; i < s.size(); i++) {
             if (s[i] == s[i-1]) {
                 curr++;
