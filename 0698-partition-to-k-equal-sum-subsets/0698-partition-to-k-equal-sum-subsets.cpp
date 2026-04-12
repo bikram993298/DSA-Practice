c52ba8851c44848c280c11448b72fae28c723a5d
sparrow <150315437+bikram993298@users.noreply.github.com>
2026-04-12 10:42:07 +0530

    Time: 0 ms (100%), Space: 12.1 MB (44.59%) - LeetHub

--- 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.cpp
+++ 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.cpp
@@ -2,27 +2,28 @@ class Solution {
 public:
     int target;
     int k_;
-    int cnt = 0;
 
-    bool solve(vector<int>& nums, vector<bool>& used, int curr, int start) {
+    bool solve(vector<int>& nums, vector<bool>& used, int curr, int start, int cnt) {
+        
         if (cnt == k_) return true;
 
         if (curr == target) {
-            cnt++;
-            if (solve(nums, used, 0, 0)) return true;
-            cnt--;
-            return false;
+            return solve(nums, used, 0, 0, cnt + 1);
         }
 
-        for (int i = start; i < (int)nums.size(); i++) {
+        for (int i = start; i < nums.size(); i++) {
             if (used[i]) continue;
             if (curr + nums[i] > target) continue;
 
             used[i] = true;
 
-            if (solve(nums, used, curr + nums[i], i + 1)) return true;
+            if (solve(nums, used, curr + nums[i], i + 1, cnt))
+                return true;
 
             used[i] = false;
+
+            // 🔥 pruning (very important)
+            if (curr == 0) return false;
         }
 
         return false;
@@ -35,13 +36,13 @@ public:
         if (sum % k != 0) return false;
 
         target = sum / k;
+
         sort(nums.rbegin(), nums.rend());
 
         if (nums[0] > target) return false;
 
         vector<bool> used(nums.size(), false);
-        cnt = 0;
 
-        return solve(nums, used, 0, 0);
+        return solve(nums, used, 0, 0, 0);
     }
 };
