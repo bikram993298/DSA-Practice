30531894be86ad2f1a73e47b71be4b57aebeb751
sparrow <150315437+bikram993298@users.noreply.github.com>
2026-04-12 12:44:00 +0530

    Time: 28 ms (34.74%), Space: 20.9 MB (80.45%) - LeetHub

--- 0690-employee-importance/0690-employee-importance.cpp
+++ 0690-employee-importance/0690-employee-importance.cpp
@@ -1,39 +1,65 @@
-/*
-// Definition for Employee.
-class Employee {
-public:
-    int id;
-    int importance;
-    vector<int> subordinates;
-};
-*/
+// /*
+// // Definition for Employee.
+// class Employee {
+// public:
+//     int id;
+//     int importance;
+//     vector<int> subordinates;
+// };
+// */
+
+// class Solution {
+// public:
+
+//     int solve(int id, vector<vector<int>>& adj, vector<int>& imp) {
+//         int ans = imp[id];
+
+//         for (auto it : adj[id]) {
+//             ans += solve(it, adj, imp);
+//         }
+
+//         return ans;
+//     }
+
+//     int getImportance(vector<Employee*> employees, int id) {
+
+//         vector<vector<int>> adj(2001);   // assuming id <= 2000
+//         vector<int> imp(2001, 0);
+
+//         for (auto it : employees) {
+//             imp[it->id] = it->importance;
+
+//             for (auto sub : it->subordinates) {
+//                 adj[it->id].push_back(sub);
+//             }
+//         }
+
+//         return solve(id, adj, imp);
+//     }
+// };
 
 class Solution {
 public:
+    unordered_map<int, Employee*> mp;
 
-    int solve(int id, vector<vector<int>>& adj, vector<int>& imp) {
-        int ans = imp[id];
+    int dfs(int id) {
+        Employee* emp = mp[id];
 
-        for (auto it : adj[id]) {
-            ans += solve(it, adj, imp);
+        int total = emp->importance;
+
+        for (int sub : emp->subordinates) {
+            total += dfs(sub);
         }
 
-        return ans;
+        return total;
     }
 
     int getImportance(vector<Employee*> employees, int id) {
-
-        vector<vector<int>> adj(2001);   // assuming id <= 2000
-        vector<int> imp(2001, 0);
-
-        for (auto it : employees) {
-            imp[it->id] = it->importance;
-
-            for (auto sub : it->subordinates) {
-                adj[it->id].push_back(sub);
-            }
+        // map id → employee
+        for (auto e : employees) {
+            mp[e->id] = e;
         }
 
-        return solve(id, adj, imp);
+        return dfs(id);
     }
 };
