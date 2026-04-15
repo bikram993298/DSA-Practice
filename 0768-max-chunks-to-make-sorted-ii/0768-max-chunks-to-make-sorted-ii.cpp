9f4fb2132f29339a778c11f7f6d90e9871c8f58d
sparrow <150315437+bikram993298@users.noreply.github.com>
2026-04-15 11:55:23 +0530

    Time: 0 ms (100%), Space: 16.2 MB (54.79%) - LeetHub

---
+++ 0768-max-chunks-to-make-sorted-ii/0768-max-chunks-to-make-sorted-ii.cpp
@@ -0,0 +1,34 @@
+
+class Solution {
+public:
+    int maxChunksToSorted(vector<int>& arr) {
+        int n = arr.size();
+        // important greedy problem beaucces in eery index we are finding thath first ka maximum should be greater than second aort ka minum if yes then chunk can be made then go ,,if it is i 0 the inex edge  case because 0 th index -1 will minimummm then last iedx suffix mean will be there
+        vector<int> prefixMax(begin(arr), end(arr));
+        vector<int> suffixMin(begin(arr), end(arr));
+
+        for (int i = 1; i < n; i++) {
+            prefixMax[i] = max(prefixMax[i - 1], prefixMax[i]);
+        }
+
+        for (int i = n - 2; i >= 0; i--) {
+            suffixMin[i] = min(suffixMin[i + 1], suffixMin[i]);
+        }
+
+        int chunksCount = 1;
+        // in deafult case there wil b eone chunking whole if no cut,,,if ther ei one cut here two chuning so this type 
+        for (int i = 1; i < n; i++) {
+            int pehleKaMax =  prefixMax[i - 1] ;
+            int baadKaMin  = suffixMin[i];
+            
+            if(pehleKaMax <= baadKaMin) {
+                chunksCount++;
+            }
+        }
+
+        return chunksCount;
+    }
+};
+
+//T.C : O(n)
+//S.C : O(n)
