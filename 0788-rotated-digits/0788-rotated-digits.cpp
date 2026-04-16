a5f30b863c26c26b2d6750a7c136d758a6ffb50e
sparrow <150315437+bikram993298@users.noreply.github.com>
2026-04-16 10:13:04 +0530

    Time: 0 ms (100%), Space: 7.8 MB (83.33%) - LeetHub

---
+++ 0788-rotated-digits/0788-rotated-digits.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool isGood(int num) {
+        bool changed = false;
+// A number is good if:
+
+// all digits are valid
+// at least one digit changes after rotation
+        while(num > 0) {
+            int d = num % 10;
+
+            // invalid digits
+            if(d == 3 || d == 4 || d == 7) return false;
+
+            // digits that change
+            if(d == 2 || d == 5 || d == 6 || d == 9) {
+                changed = true;
+            }
+
+            num /= 10;
+        }
+
+        return changed;
+    }
+
+    int rotatedDigits(int n) {
+        int count = 0;
+
+        for(int i = 1; i <= n; i++) {
+            if(isGood(i)) count++;
+        }
+
+        return count;
+    }
+};
