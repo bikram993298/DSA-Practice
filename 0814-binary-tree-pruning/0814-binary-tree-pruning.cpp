c5f01867817cebcda227161a770dfc3671a93054
sparrow <150315437+bikram993298@users.noreply.github.com>
2026-04-16 10:31:38 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0814-binary-tree-pruning/0814-binary-tree-pruning.cpp
+++ 0814-binary-tree-pruning/0814-binary-tree-pruning.cpp
@@ -1,24 +1,42 @@
+// class Solution {
+// public:
+
+//     int solve(TreeNode* root) {
+//         if (!root) return 0;
+
+//         int left = solve(root->left);
+//         int right = solve(root->right);
+
+//         // prune left
+//         if (left == 0) root->left = NULL;
+
+//         // prune right
+//         if (right == 0) root->right = NULL;
+
+//         // total number of 1s in subtree
+//         return left + right + (root->val == 1);
+//     }
+
+//     TreeNode* pruneTree(TreeNode* root) {
+//         if (solve(root) == 0) return NULL;
+//         return root;
+//     }
+// };
+
+// pr we can say delete laf nodes dynam,icaaly which have zero 
 class Solution {
 public:
-
-    int solve(TreeNode* root) {
-        if (!root) return 0;
-
-        int left = solve(root->left);
-        int right = solve(root->right);
-
-        // prune left
-        if (left == 0) root->left = NULL;
-
-        // prune right
-        if (right == 0) root->right = NULL;
-
-        // total number of 1s in subtree
-        return left + right + (root->val == 1);
-    }
-
     TreeNode* pruneTree(TreeNode* root) {
-        if (solve(root) == 0) return NULL;
+        if (!root) return NULL;
+
+        root->left = pruneTree(root->left);
+        root->right = pruneTree(root->right);
+
+        // if current node becomes useless
+        if (!root->left && !root->right && root->val == 0) {
+            return NULL;
+        }
+
         return root;
     }
 };
