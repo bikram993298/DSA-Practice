c35649b2669c2cb3b6d6df10ae44360df29d4635
sparrow <150315437+bikram993298@users.noreply.github.com>
2026-04-16 12:09:01 +0530

    Time: 941 ms (15.02%), Space: 178.8 MB (16.44%) - LeetHub

---
+++ 0786-k-th-smallest-prime-fraction/0786-k-th-smallest-prime-fraction.cpp
@@ -0,0 +1,63 @@
+
+// class Solution {
+// public:
+//     vector<int> kthSmallestPrimeFraction(vector<int>& arr, int k) {
+//         priority_queue<vector<double>> pq;
+
+
+// //Approach-1 (Using simple Heap solution as it's asking for kth smallest)
+// //T.C : O(n^2 * log(k))
+// //S.C : O(k)
+//         for (int i = 0; i < arr.size(); i++) {
+//             for (int j = i+1; j < arr.size(); j++) {
+//                 double div = (double) arr[i] / arr[j];
+//                 pq.push(vector<double>{div, (double)arr[i], (double)arr[j]});
+//                 if(pq.size() > k)
+//                     pq.pop();
+//             }
+//         }
+
+//         auto vec  = pq.top();
+//         vector<int> result(2);
+//         result[0] = vec[1];
+//         result[1] = vec[2];
+//         return result;
+//     }
+// };
+
+
+//Approach-2 (Optimising Approach-1)
+//T.C : O((n+k)logn)
+//S.C : O(n)
+class Solution {
+public:
+    typedef vector<double> V;
+    // in  tghis appriach what you are doing pushing smallest elemnts,devidind by vector.back macimum denomitor,,now what is smallest you got in pq,,then its next will be pushed,if k smallest then pop k-1 times,,pq,top will gie you ans
+    vector<int> kthSmallestPrimeFraction(vector<int>& arr, int k) {
+        int n = arr.size();
+        priority_queue<V, vector<V>, greater<V>> pq;
+        
+        for(int i = 0; i < n; i++)
+            pq.push({1.0*arr[i]/arr.back(), (double)(i), (double)(n-1)});
+        
+        int smallest = 1;
+        
+        while(smallest < k) {
+            V vec = pq.top();
+            pq.pop();
+             smallest++;
+            int i = vec[1];
+            int j = vec[2]-1;
+
+            
+            pq.push({1.0*arr[i]/arr[j], (double)(i), (double)(j)});
+           
+        }
+        
+        V vec = pq.top();
+        int i = vec[1];
+        int j = vec[2];
+        return {arr[i], arr[j]};
+    }
+};
+
