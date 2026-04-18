6a6cfa5e2a43bb13e02d11f21ae0d1a7375a512b
sparrow <150315437+bikram993298@users.noreply.github.com>
2026-04-18 09:25:52 +0530

    Time: 4 ms (54.44%), Space: 31.9 MB (68.1%) - LeetHub

--- 0173-binary-search-tree-iterator/0173-binary-search-tree-iterator.cpp
+++ 0173-binary-search-tree-iterator/0173-binary-search-tree-iterator.cpp
@@ -1,26 +1,66 @@
+// class BSTIterator {
+// private:
+//     vector<int> inorder;
+//     int index = 0;
+
+//     void traverse(TreeNode* root) {
+//         if (!root) return;
+
+//         traverse(root->left);
+//         inorder.push_back(root->val);
+//         traverse(root->right);
+//     }
+
+// public:
+//     BSTIterator(TreeNode* root) {
+//         traverse(root);
+//     }
+
+//     int next() {
+//         return inorder[index++];
+//     }
+
+//     bool hasNext() {
+//         return index < inorder.size();
+//     }
+// };
+// extra o(n) space 
+// here rwhat we are dong that we are itertavie inorder traversal we are doing you are inserting alll elft elemnts then last lelemnt hten if right exist push on the stack ,,has next till stakc is no tempty
+
 class BSTIterator {
 private:
-    vector<int> inorder;
-    int index = 0;
+    stack<TreeNode*> st;
 
-    void traverse(TreeNode* root) {
-        if (!root) return;
-
-        traverse(root->left);
-        inorder.push_back(root->val);
-        traverse(root->right);
+    // Push all left nodes into stack
+    void pushLeft(TreeNode* node) {
+        while (node != NULL) {
+            st.push(node);
+            node = node->left;
+        }
     }
 
 public:
+    // Constructor
     BSTIterator(TreeNode* root) {
-        traverse(root);
+        pushLeft(root);
     }
 
+    // Return next smallest
     int next() {
-        return inorder[index++];
+        TreeNode* node = st.top();
+        st.pop();
+
+        // If right subtree exists, process it
+        if (node->right != NULL) {
+            pushLeft(node->right);
+        }
+
+        return node->val;
     }
 
+    // Check if more elements exist
     bool hasNext() {
-        return index < inorder.size();
+        return !st.empty();
     }
 };
+//o(h)
