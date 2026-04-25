aa24ef030dc92953bed9b41e2589acf7a5fc337d
sparrow <150315437+bikram993298@users.noreply.github.com>
2026-04-25 10:36:38 +0530

    Time: 34 ms (5.43%), Space: 38.2 MB (5.1%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.cpp
+++ 1029-two-city-scheduling/1029-two-city-scheduling.cpp
@@ -1,36 +1,42 @@
 class Solution {
 public:
     int n;
-    vector<vector<int>> dp;
+    vector<vector<vector<int>>> dp;
 
-    int solve(int i, int a, vector<vector<int>>& costs) {
-        if (i == costs.size())
-            return 0;
+    int solve(int i, int a, int b, vector<vector<int>>& costs) {
+        
+        if (i == costs.size()) {
+            if (a == n && b == n)
+                return 0;
+            return 1e9;
+        }
 
-        if (dp[i][a] != -1)
-            return dp[i][a];
+        if (dp[i][a][b] != -1)
+            return dp[i][a][b];
 
-        int sendA = INT_MAX;
-        int sendB = INT_MAX;
+        int sendA = 1e9;
+        int sendB = 1e9;
 
-        // send to city A
         if (a < n) {
-            sendA = costs[i][0] + solve(i + 1, a + 1, costs);
+            sendA = costs[i][0] + solve(i + 1, a + 1, b, costs);
         }
 
-        // people sent to B = i - a
-        if ((i - a) < n) {
-            sendB = costs[i][1] + solve(i + 1, a, costs);
+        if (b < n) {
+            sendB = costs[i][1] + solve(i + 1, a, b + 1, costs);
         }
 
-        return dp[i][a] = min(sendA, sendB);
+        return dp[i][a][b] = min(sendA, sendB);
     }
 
     int twoCitySchedCost(vector<vector<int>>& costs) {
         n = costs.size() / 2;
 
-        dp.resize(costs.size(), vector<int>(n + 1, -1));
+        int total = costs.size();
 
-        return solve(0, 0, costs);
+        dp.resize(total,
+                  vector<vector<int>>(n + 1,
+                  vector<int>(n + 1, -1)));
+
+        return solve(0, 0, 0, costs);
     }
 };
