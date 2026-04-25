24972f2579714f5ba854d4d2cf4b772bbb254d46
sparrow <150315437+bikram993298@users.noreply.github.com>
2026-04-25 10:43:49 +0530

    Time: 3 ms (17.52%), Space: 12.8 MB (8.43%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.cpp
+++ 1029-two-city-scheduling/1029-two-city-scheduling.cpp
@@ -1,45 +1,93 @@
+// class Solution {
+// public:
+//     int n;   // total number of people
+//     vector<vector<vector<int>>> dp;
+
+//     int solve(int i, int a, int b, vector<vector<int>>& costs) {
+
+//         // all people processed
+//         if (i == n) {
+//             if (a == n / 2 && b == n / 2)
+//                 return 0;
+
+//             return 1e9; 
+//         }
+
+//         if (dp[i][a][b] != -1)
+//             return dp[i][a][b];
+
+//         int sendA = 1e9;
+//         int sendB = 1e9;
+
+       
+//         if (a < n / 2) {
+//             sendA = costs[i][0] + solve(i + 1, a + 1, b, costs);
+//         }
+
+     
+//         if (b < n / 2) {
+//             sendB = costs[i][1] + solve(i + 1, a, b + 1, costs);
+//         }
+
+//         return dp[i][a][b] = min(sendA, sendB);
+//     }
+
+//     int twoCitySchedCost(vector<vector<int>>& costs) {
+
+//         n = costs.size(); // total people
+
+//         dp.resize(n,
+//                   vector<vector<int>>(n + 1,
+//                   vector<int>(n + 1, -1)));
+
+//         return solve(0, 0, 0, costs);
+//     }
+// };
 class Solution {
 public:
-    int n;   // total number of people
-    vector<vector<vector<int>>> dp;
+    int n;   // total people / 2
+    vector<vector<int>> dp;
 
-    int solve(int i, int a, int b, vector<vector<int>>& costs) {
+    int solve(int i, int a, vector<vector<int>>& costs) {
 
         // all people processed
-        if (i == n) {
-            if (a == n / 2 && b == n / 2)
-                return 0;
+        if (i == costs.size()){
+            if (a == n / 2 && (i-a) == n / 2){
+                 return 0;
 
-            return 1e9; 
-        }
+            }
+               
 
-        if (dp[i][a][b] != -1)
-            return dp[i][a][b];
+             return 1e9; 
+       }
+        
+           
+
+        if (dp[i][a] != -1)
+            return dp[i][a];
 
         int sendA = 1e9;
         int sendB = 1e9;
 
-       
-        if (a < n / 2) {
-            sendA = costs[i][0] + solve(i + 1, a + 1, b, costs);
+        // send to city A
+        if (a < n/2) {
+            sendA = costs[i][0] + solve(i + 1, a + 1, costs);
         }
 
-     
-        if (b < n / 2) {
-            sendB = costs[i][1] + solve(i + 1, a, b + 1, costs);
+        // people already sent to B = i - a
+        if ((i - a) < n/2) {
+            sendB = costs[i][1] + solve(i + 1, a, costs);
         }
 
-        return dp[i][a][b] = min(sendA, sendB);
+        return dp[i][a] = min(sendA, sendB);
     }
 
     int twoCitySchedCost(vector<vector<int>>& costs) {
 
-        n = costs.size(); // total people
+        n = costs.size();
 
-        dp.resize(n,
-                  vector<vector<int>>(n + 1,
-                  vector<int>(n + 1, -1)));
+        dp.resize(costs.size(), vector<int>(n + 1, -1));
 
-        return solve(0, 0, 0, costs);
+        return solve(0, 0, costs);
     }
 };
