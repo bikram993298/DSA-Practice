2c595ef22bbb75d3a4f36e2c076ed5b9e1da8911
sparrow <150315437+bikram993298@users.noreply.github.com>
2026-05-01 13:56:54 +0530

    Time: 3 ms (66.6%), Space: 15.1 MB (9.66%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,24 +1,39 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        //o(nlogn)
-           int n=nums.size();
-        vector<pair<int,int>> p;
-        for(int i=0;i<n;i++){
-            p.push_back({nums[i],i});
-        }
-        sort(p.begin(),p.end());
+        // //o(nlogn)
+        // // sapce o(n)
+        //    int n=nums.size();
+        // vector<pair<int,int>> p;
+        // for(int i=0;i<n;i++){
+        //     p.push_back({nums[i],i});
+        // }
+        // sort(p.begin(),p.end());
      
-        int l=0,r=n-1;
-        while(l<r){
-            int sum=p[l].first+p[r].first;
-            if(sum==target) return {p[l].second,p[r].second};
-            if(sum>target){
+        // int l=0,r=n-1;
+        // while(l<r){
+        //     int sum=p[l].first+p[r].first;
+        //     if(sum==target) return {p[l].second,p[r].second};
+        //     if(sum>target){
 
-                r--;
-            }else{
-                l++;
+        //         r--;
+        //     }else{
+        //         l++;
+        //     }
+        // }
+        // return {-1,-1};
+        int n=nums.size();
+
+        map<int,int>mp;
+        //i=target-j
+        for(int i=0;i<n;i++){
+            if(mp.count(target-nums[i])){
+                return {i,mp[target-nums[i]]};
             }
+            //mp[3]=0;
+            //mp[2]=1;
+
+            mp[nums[i]]=i;
         }
         return {-1,-1};
     }
