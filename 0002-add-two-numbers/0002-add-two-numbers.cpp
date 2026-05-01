dbd3d18752d6b9285e97d7fbaef8a23386cd0a3f
sparrow <150315437+bikram993298@users.noreply.github.com>
2026-05-01 21:58:37 +0530

    Time: 0 ms (100%), Space: 77.1 MB (43.85%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -1,39 +1,63 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
         ListNode* dummy=new ListNode(0);
-        ListNode* curr=dummy; 
+         
 
-        int carry=0;
+         int carry=0;
+         ListNode* head=dummy;
 
-        while(l1 && l2){
-            int sum=l1->val+l2->val+carry;
+         while(l1!=NULL && l2!=NULL){
+            int nums=l1->val+l2->val;
+            int base=(nums+carry)%10;
+           
+            ListNode* temp=new ListNode(base);
+             carry=(nums+carry)/10;
+            head->next=temp;
             l1=l1->next;
             l2=l2->next;
+            head=head->next;
+            
 
-            curr->next=new ListNode(sum%10);
-            carry=sum/10;
-            curr=curr->next;
-        }
-
-        while(l1){
-            int sum=l1->val+carry;
-            curr->next=new ListNode(sum%10);
-            carry=sum/10;
-            curr=curr->next;
+         }
+         while(l1!=NULL){
+            int base=(l1->val+carry)%10;
+           
+            ListNode* temp=new ListNode(base);
+             carry=(l1->val+carry)/10;
+            head->next=temp;
+            head=head->next;
             l1=l1->next;
-        }
 
-        while(l2){
-            int sum=l2->val+carry;
-            curr->next=new ListNode(sum%10);
-            carry=sum/10;
-            curr=curr->next;
+         }
+          while(l2!=NULL){
+            int base=(l2->val+carry)%10;
+           
+            ListNode* temp=new ListNode(base);
+             carry=(l2->val+carry)/10;
+            head->next=temp;
+            head=head->next;
             l2=l2->next;
-        }
 
-        if(carry) curr->next=new ListNode(carry);
+         }
+         if(carry!=0){
+            head->next=new ListNode(carry);
+         }
 
-        return dummy->next;
+
+
+         return dummy->next;
+
+        
     }
 };
