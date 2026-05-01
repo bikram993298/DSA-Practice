84cc9100f08417840e447703c8bfc7639b02d8c9
sparrow <150315437+bikram993298@users.noreply.github.com>
2026-05-01 11:19:26 +0530

    Time: 11 ms (8.41%), Space: 104.2 MB (14.15%) - LeetHub

--- 0396-rotate-function/0396-rotate-function.cpp
+++ 0396-rotate-function/0396-rotate-function.cpp
@@ -1,39 +1,37 @@
 class Solution {
 public:
-    long long base_sum = 0;
-    long long sum = 0;
-    long long max_sum;
+    int ans;
+    int base_sum=0;
+    int sum=0;
+    int max_sum=0;
     int n;
+    int solve(int k,vector<int> &  nums){
+   if(k==0){
+    return base_sum;
+           
+   }
+   int ree=solve(k-1,nums)+sum-n*nums[n-k];
+   max_sum=max(max_sum,ree);
 
-    long long solve(int k, vector<int> &nums) {
-        if (k == 0) {
-            return base_sum;
-        }
 
-        long long prev = solve(k - 1, nums);
+   return ree;
 
-        long long curr = prev + sum - (long long)n * nums[n - k];
 
-        max_sum = max(max_sum, curr);
-
-        return curr;
     }
-
     int maxRotateFunction(vector<int>& nums) {
-        n = nums.size();
+        n=nums.size();
 
-        base_sum = 0;
-        sum = 0;
 
-        for (int i = 0; i < n; i++) {
-            base_sum += (long long)i * nums[i];
-            sum += nums[i];
+        for(int i=0;i<n;i++){
+            base_sum+=(i*nums[i]);
+            sum+=nums[i];
         }
+        max_sum=base_sum;
+      int ans1=  solve(n-1,nums);
+        return max_sum;
 
-        max_sum = base_sum; // include F(0)
+        
 
-        solve(n - 1, nums);
-
-        return (int)max_sum;
+        
     }
 };
