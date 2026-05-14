657087e02075ef3cb12a6df2b3ced3e822581d71
sparrow <150315437+bikram993298@users.noreply.github.com>
2026-05-14 14:13:47 +0530

    Time: 0 ms (100%), Space: 23.1 MB (77.16%) - LeetHub

--- 1163-last-substring-in-lexicographical-order/1163-last-substring-in-lexicographical-order.cpp
+++ 1163-last-substring-in-lexicographical-order/1163-last-substring-in-lexicographical-order.cpp
@@ -2,7 +2,8 @@ class Solution {
 public:
     string lastSubstring(string s) {
         int i = 0, j = 1, k = 0;
-
+// main idea is here we have to compare substring from i and j becuse if we consider prefix starting form j it wil definately subtring will have bigger so compare only subctring by twwo pointer now i will be 0 then jwill be i+1 ,,maintain this if somehow aab
+//aac c is dieffernt so aab's a,b will neveer createt any suffix that will be bigger than substring starting aac,,because c can be more than them all substrings,,we can skip i+k now 
         while (j + k < s.size()) {
             if (s[i + k] == s[j + k]) {
                 k++;
