cf7aa09e0f8b31cc65abe266df45b7b574215adc
sparrow <150315437+bikram993298@users.noreply.github.com>
2026-05-16 14:24:55 +0530

    Time: 27 ms (80.51%), Space: 14.6 MB (67.51%) - LeetHub

---
+++ 1473-paint-house-iii/1473-paint-house-iii.cpp
@@ -0,0 +1,64 @@
+class Solution {
+public:
+    const int INF = 1e9;
+
+    int dp[101][21][101];
+
+    int solve(int idx, int prevColor, int groups, vector<int>& houses,
+              vector<vector<int>>& cost, int m, int n, int target) {
+
+        // too many neighborhoods
+        if (groups > target)
+            return INF;
+
+        // all houses processed
+        if (idx == m) {
+
+            if (groups == target)
+                return 0;
+
+            return INF;
+        }
+
+        // memoization
+        if (dp[idx][prevColor][groups] != -1)
+            return dp[idx][prevColor][groups];
+
+        int ans = INF;
+
+        // already painted
+        if (houses[idx] != 0) {
+
+            int color = houses[idx];
+
+            int newGroups = groups + (color != prevColor);
+
+            ans = solve(idx + 1, color, newGroups, houses, cost, m, n, target);
+        }
+
+        // not painted
+        else {
+
+            for (int color = 1; color <= n; color++) {
+
+                int newGroups = groups + (color != prevColor);
+
+                ans = min(ans, cost[idx][color - 1] +
+                                   solve(idx + 1, color, newGroups, houses,
+                                         cost, m, n, target));
+            }
+        }
+
+        return dp[idx][prevColor][groups] = ans;
+    }
+
+    int minCost(vector<int>& houses, vector<vector<int>>& cost, int m, int n,
+                int target) {
+
+        memset(dp, -1, sizeof(dp));
+
+        int ans = solve(0, 0, 0, houses, cost, m, n, target);
+
+        return (ans >= INF ? -1 : ans);
+    }
+};
