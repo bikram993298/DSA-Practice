60a0d5a7c37c47a27e336704667c57f892bc3227
sparrow <150315437+bikram993298@users.noreply.github.com>
2026-05-16 18:20:00 +0530

    Time: 99 ms (7.56%), Space: 96 MB (8.71%) - LeetHub

--- 1472-design-browser-history/1472-design-browser-history.cpp
+++ 1472-design-browser-history/1472-design-browser-history.cpp
@@ -32,6 +32,7 @@ public:
     // Overshot — restore oldest page as the floor
 
     // if size is 1 and back 2 that meansyou should return 1,and the backst,it should keep last atleast one lemnt
+    //if backst,size()=1 and beack 2 then what will happwn it will give 1 page not chnage
     if (backSt.empty()) {
         backSt.push(forwardSt.top());  // ✅ put homepage back
         forwardSt.pop();
