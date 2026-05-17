e2cb1ae959aed10c97091f7b76d2ac5e395e6d04
sparrow <150315437+bikram993298@users.noreply.github.com>
2026-05-17 20:32:13 +0530

    Time: 0 ms (100%), Space: 8.4 MB (37.28%) - LeetHub

---
+++ 1328-break-a-palindrome/1328-break-a-palindrome.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string breakPalindrome(string palindrome) {
+
+        int n = palindrome.size();
+
+        // single char
+        if (n == 1)
+            return "";
+
+        // check first half
+        for (int i = 0; i < n / 2; i++) {
+
+            if (palindrome[i] != 'a') {
+
+                palindrome[i] = 'a';
+
+                return palindrome;
+            }
+        }
+
+        // all 'a'
+        palindrome[n - 1] = 'b';
+
+        return palindrome;
+    }
+};
