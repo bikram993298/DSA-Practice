a768f61e03b60ab05cf88cf39f054f27864a2a25
sparrow <150315437+bikram993298@users.noreply.github.com>
2026-05-17 08:00:36 +0530

    Time: 2 ms (2.2%), Space: 8.5 MB (7.82%) - LeetHub

---
+++ 1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.cpp
@@ -0,0 +1,60 @@
+// class Solution {
+// public:
+//     int isPrefixOfWord(string sentence, string searchWord) {
+//         // i will traverse thorght he string and i will create map where each possible prefix i will store,and for making each possible prefix i have to creater another epmty string ,,when i encounter space i will makeit empty agian then, if not space add in the same string ,,ehn space push map and make empty string ,,,now chek burg exist or not
+        
+//     }
+// };
+class Solution {
+public:
+    int isPrefixOfWord(string sentence, string searchWord) {
+
+        map<string,int> mp;
+
+        string temp = "";
+
+        int idx = 1;
+
+        for (int i = 0; i < sentence.size(); i++) {
+
+            if (sentence[i] == ' ') {
+
+                string prefix = "";
+
+                // store all prefixes
+                for (int j = 0; j < temp.size(); j++) {
+
+                    prefix += temp[j];
+
+                    // store first occurrence only
+                    if (mp.find(prefix) == mp.end())
+                        mp[prefix] = idx;
+                }
+
+                idx++;
+
+                temp = "";
+            }
+            else {
+
+                temp += sentence[i];
+            }
+        }
+
+        // last word process
+        string prefix = "";
+
+        for (int j = 0; j < temp.size(); j++) {
+
+            prefix += temp[j];
+
+            if (mp.find(prefix) == mp.end())
+                mp[prefix] = idx;
+        }
+
+        if (mp.find(searchWord) != mp.end())
+            return mp[searchWord];
+
+        return -1;
+    }
+};
