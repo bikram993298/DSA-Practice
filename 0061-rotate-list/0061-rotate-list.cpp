7388cb2a17ad2c4e3b1eefba9492d1d5df3f7ac5
sparrow <150315437+bikram993298@users.noreply.github.com>
2026-05-22 00:07:11 +0530

    Time: 0 ms (100%), Space: 16.3 MB (62.4%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.cpp
+++ 0061-rotate-list/0061-rotate-list.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     ListNode* rotateRight(ListNode* head, int k) {
+        // base case you should remember
         if(!head || !head->next || k == 0) return head;
 
         int n = 1;
@@ -17,9 +18,13 @@ public:
         tail->next = head; // make circular
 
         int steps = n - k;
-        ListNode* newTail = head;
+        //  we have to break n-k bond and 
 
-        for(int i = 1; i < steps; i++)
+        ListNode* newTail = head;
+        // lets think 3 ke bad bond delete karna he so 
+        // 1->2->3->4->5 last se 2 roroate means 4-> 5 will forwaard n-2 means ,if u want to go 3 then only move 3-1  times
+
+        for(int i = 0; i < steps-1; i++)
             newTail = newTail->next;
 
         ListNode* newHead = newTail->next;
