c6fb509f578d99712cff7a55ed75970275ff7cd9
sparrow <150315437+bikram993298@users.noreply.github.com>
2026-05-21 17:10:44 +0530

    Time: 8 ms (83.43%), Space: 11.7 MB (96.94%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -7,6 +7,8 @@ public:
         ListNode* slow = head;
         ListNode* fast = head;
         // necause you are going fast->next-> next so you hae to chekc fst->next should exicts to overcoem segmentatio so this cehck..alwasy use chek what yo are accesign accoridn t othat access
+        // resaon is if there is cicirular loop then fast and fast nest never null and it slow and fast both wil l enter circle contutnue circural rtrack now fast going 2 times so it willdefiantely catch,,
+        // othersies no loop fast will go 2 usnit and eventually raweach edn
 
         while (fast && fast->next) {
             slow = slow->next;
