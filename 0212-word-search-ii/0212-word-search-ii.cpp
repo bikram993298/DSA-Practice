b045ef7bd0813266f0d9c055481a59f18b3949b0
sparrow <150315437+bikram993298@users.noreply.github.com>
2026-05-25 23:59:18 +0530

    Time: 526 ms (40.61%), Space: 19.9 MB (37.86%) - LeetHub

--- 0212-word-search-ii/0212-word-search-ii.cpp
+++ 0212-word-search-ii/0212-word-search-ii.cpp
@@ -1,77 +1,104 @@
-struct TrieNode {
+class TrieNode {
 public:
     TrieNode* children[26];
-    string word;  // stores word if this node is end
+    string word;
 
     TrieNode() {
-        for (int i = 0; i < 26; i++) children[i] = NULL;
+
+        for (int i = 0; i < 26; i++) {
+            children[i] = NULL;
+        }
+
         word = "";
     }
 };
 
 class Solution {
 public:
-    vector<string> result;
-
     void insert(TrieNode* root, string& word) {
+
         TrieNode* node = root;
-        for (char c : word) {
-            if (!node->children[c - 'a'])
-                node->children[c - 'a'] = new TrieNode();
-            node = node->children[c - 'a'];
+
+        for (char ch : word) {
+
+            int idx = ch - 'a';
+
+            if (node->children[idx] == NULL) {
+                node->children[idx] = new TrieNode();
+            }
+
+            node = node->children[idx];
         }
-        node->word = word;  // mark end
+
+        node->word = word;
     }
 
-    void dfs(vector<vector<char>>& board, int i, int j, TrieNode* node) {
-        char c = board[i][j];
+    void dfs(int i, int j, vector<vector<char>>& board, TrieNode* node,
+             vector<string>& ans) {
 
-        // Not in Trie
-        if (c == '#' || node->children[c - 'a'] == NULL)
+        char ch = board[i][j];
+
+        // prefix does not exist
+        if (node->children[ch - 'a'] == NULL) {
             return;
-
-        node = node->children[c - 'a'];
-
-        // Found word
-        if (node->word != "") {
-            result.push_back(node->word);
-            node->word = "";  // avoid duplicates
         }
 
-        // Mark visited
+        node = node->children[ch - 'a'];
+           // mark visited
         board[i][j] = '#';
-        //you are using this as visisted so in next call there boar i j, can be # thhis if this check return ,,,
 
-        int dirs[4][2] = {{0,1},{1,0},{0,-1},{-1,0}};
+        // word found
+        if (node->word != "") {
+
+            ans.push_back(node->word);
+            // like you have got string bikram then if  somehow you got another root that givinf same for that remove string word from tree by making blank
+            // avoid duplicate insertion
+            node->word = "";
+        }
+
+     
+
+        int dirs[4][2] = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+
         for (auto& d : dirs) {
-            int ni = i + d[0], nj = j + d[1];
 
-            if (ni >= 0 && nj >= 0 && ni < board.size() && nj < board[0].size()) {
-                dfs(board, ni, nj, node);
+            int ni = i + d[0];
+            int nj = j + d[1];
+
+            if (ni >= 0 && nj >= 0 && ni < board.size() &&
+                nj < board[0].size() && board[ni][nj] != '#') {
+
+                dfs(ni, nj, board, node, ans);
             }
         }
 
-        // Backtrack
-        board[i][j] = c;
+        // backtrack
+        board[i][j] = ch;
     }
 
-    vector<string> findWords(vector<vector<char>>& board, vector<string>& words) {
+    vector<string> findWords(vector<vector<char>>& board,
+                             vector<string>& words) {
 
         TrieNode* root = new TrieNode();
 
-        // Build Trie
+        // build trie
         for (string& word : words) {
             insert(root, word);
         }
 
-        // Start DFS from every cell
-        for (int i = 0; i < board.size(); i++) {
-            for (int j = 0; j < board[0].size(); j++) {
-                dfs(board, i, j, root);
+        vector<string> ans;
+
+        int m = board.size();
+        int n = board[0].size();
+
+        for (int i = 0; i < m; i++) {
+
+            for (int j = 0; j < n; j++) {
+
+                dfs(i, j, board, root, ans);
             }
         }
 
-        return result;
+        return ans;
     }
 };
-//o(m*n*4^l)
