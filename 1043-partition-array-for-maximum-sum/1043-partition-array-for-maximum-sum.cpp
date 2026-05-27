7e476cb157d94c2691bc8e04ce97dfa15cd5e1ca
sparrow <150315437+bikram993298@users.noreply.github.com>
2026-05-28 01:07:14 +0530

    Time: 6 ms (56.31%), Space: 12.5 MB (29.15%) - LeetHub

--- 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
@@ -1,30 +1,50 @@
 class Solution {
 public:
-    int n;
-    int t[501];
-    int solve(vector<int>& arr, int idx, int k) {
-        if(idx >= n) {
+    int solve(int i, vector<int>& arr, int k, vector<int>& dp) {
+
+        int n = arr.size();
+
+        // End reached
+        if (i == n)
             return 0;
+
+        // Memoization
+        if (dp[i] != -1)
+            return dp[i];
+
+        int maxi = 0;
+
+        int maxAns = 0;
+
+        int len = 0;
+
+        // Try partitions
+        for (int j = i; j < min(i + k, n); j++) {
+
+            len++;
+
+            maxi = max(maxi, arr[j]);
+
+            int sum =
+
+                (maxi * len)
+
+                +
+
+                solve(j + 1, arr, k, dp);
+
+            maxAns = max(maxAns, sum);
         }
-        
-        if(t[idx] != -1) {
-            return t[idx];
-        }
-        
-        int result = 0;
-        int curr_max = -1;
-        for(int i = idx; i < n && i-idx+1 <= k; i++) {
-            curr_max = max(curr_max, arr[i]);
-            
-            result = max(result, curr_max*(i-idx+1) + solve(arr, i+1, k));
-        }
-        
-        return t[idx] = result;
+
+        return dp[i] = maxAns;
     }
-    
+
     int maxSumAfterPartitioning(vector<int>& arr, int k) {
-        n = arr.size();
-        memset(t, -1, sizeof(t));
-        return solve(arr, 0, k);
+
+        int n = arr.size();
+
+        vector<int> dp(n, -1);
+
+        return solve(0, arr, k, dp);
     }
 };
