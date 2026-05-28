b7d2a2ad0acdb8e40f02ddfdb96438d89d80ba78
sparrow <150315437+bikram993298@users.noreply.github.com>
2026-05-28 16:54:37 +0530

    Time: 14 ms (52.9%), Space: 25.9 MB (22.42%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,30 +1,51 @@
 class Solution {
 public:
-    int solve(int i,int target,vector<int>& nums,vector<vector<int>>&dp){
-        if(i==0){  
-            if(target==0 && nums[0]==0) return 2; // +0 or -0
-            if(target==0 || target==nums[0]) return 1;
+    int S;
+
+    int solve(vector<int>& nums, int target, int i, vector<vector<int>>& dp) {
+        // Out of possible range
+        if (target > S || target < -S)
+            return 0;
+        // Used all elements
+        if (i == nums.size()) {
+
+            // Successfully formed target
+            if (target == 0)
+                return 1;
+
             return 0;
         }
-        if(dp[i][target]!=-1) return dp[i][target];
-        
-        if(nums[i]<=target){
-            return dp[i][target] = solve(i-1,target-nums[i],nums,dp) 
-                                 + solve(i-1,target,nums,dp);
-        } else {
-            return dp[i][target] = solve(i-1,target,nums,dp);
-        }
+
+        // Memoization
+        if (dp[i][target + S] != INT_MIN)
+            return dp[i][target + S];
+
+        // Put '+' before nums[i]
+        int plus = solve(nums, target - nums[i], i + 1, dp);
+
+        // Put '-' before nums[i]
+        int minus = solve(nums, target + nums[i], i + 1, dp);
+
+        return dp[i][target + S] = plus + minus;
     }
 
     int findTargetSumWays(vector<int>& nums, int target) {
-        int sum = accumulate(nums.begin(),nums.end(),0);
-        
-        if((sum+target)%2 != 0 || abs(target) > sum) return 0;
 
         int n = nums.size();
-        int subsetSum = (sum+target)/2;
 
-        vector<vector<int>> dp(n,vector<int>(subsetSum+1,-1));
-        return solve(n-1,subsetSum,nums,dp);
+        // Total sum
+        S = accumulate(nums.begin(), nums.end(), 0);
+
+        /*
+            target range:
+            [-S ... +S]
+
+            Shift by +S
+            to avoid negative indexing
+        */
+
+        vector<vector<int>> dp(n, vector<int>(2 * S + 1, INT_MIN));
+
+        return solve(nums, target, 0, dp);
     }
 };
